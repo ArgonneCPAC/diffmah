36cbbbd4865cfee9303e62ec327684ef255b7e36
Andrew Hearin <ahearin@anl.gov>
2020-02-19 23:36:49 -0600

    Improved argument handling in sigmoid_mah

--- diffmah/sigmoid_mah.py
+++ diffmah/sigmoid_mah.py
@@ -1,9 +1,23 @@
 """
 """
 import numpy as np
+from collections import OrderedDict
+
+__all__ = ("median_logmpeak_from_logt", "logmpeak_from_logt")
 
 
-def median_logmpeak_from_logt(logt, logmpeak_at_t0, logt0=1.14):
+DEFAULT_MAH_PARAMS = OrderedDict(
+    logtk=3,
+    dlogm_height=5,
+    logtc_logm0=8.3,
+    logtc_k=0.4,
+    logtc_ymin=1.635,
+    logtc_ymax=-0.365,
+)
+LOGT0 = 1.14
+
+
+def median_logmpeak_from_logt(logt, logmpeak_at_t0, logt0=LOGT0, **kwargs):
     """Median halo mass growth vs cosmic time.
 
     Parameters
@@ -25,14 +39,14 @@ def median_logmpeak_from_logt(logt, logmpeak_at_t0, logt0=1.14):
 
     """
     logt, logmpeak_at_t0 = _get_1d_arrays(logt, logmpeak_at_t0)
-    logtc, logtk, dlogm_height = _median_mah_sigmoid_params(logmpeak_at_t0)
+    logtc, logtk, dlogm_height = _median_mah_sigmoid_params(logmpeak_at_t0, **kwargs)
     dlogm = _sigmoid(logt, logtc, logtk, -dlogm_height, 0)
     dlogm_at_logt0 = _sigmoid(logt0, logtc, logtk, -dlogm_height, 0)
     logmpeak = logmpeak_at_t0 + (dlogm - dlogm_at_logt0)
     return logmpeak
 
 
-def logmpeak_from_logt(logt, logtc, k, dlogm_height, logmpeak_at_logt0, logt0):
+def logmpeak_from_logt(logt, logtc, logtk, dlogm_height, logmpeak_at_logt0, logt0):
     """Parametric model for halo mass growth vs cosmic time.
 
     Parameters
@@ -44,7 +58,7 @@ def logmpeak_from_logt(logt, logtc, k, dlogm_height, logmpeak_at_logt0, logt0):
         Base-10 log of the critical time in Gyr.
         Smaller values of logtc produce halos with earlier formation times.
 
-    k : float or ndarray
+    logtk : float or ndarray
         Steepness of transition from fast- to slow-accretion regimes.
         Larger values of k produce quicker-transitioning halos.
 
@@ -63,8 +77,8 @@ def logmpeak_from_logt(logt, logtc, k, dlogm_height, logmpeak_at_logt0, logt0):
         Base-10 log of halo mass in Msun/h
 
     """
-    dlogm = _sigmoid(logt, logtc, k, -dlogm_height, 0)
-    dlogm_at_logt0 = _sigmoid(logt0, logtc, k, -dlogm_height, 0)
+    dlogm = _sigmoid(logt, logtc, logtk, -dlogm_height, 0)
+    dlogm_at_logt0 = _sigmoid(logt0, logtc, logtk, -dlogm_height, 0)
     logmpeak = logmpeak_at_logt0 + (dlogm - dlogm_at_logt0)
     return logmpeak
 
@@ -74,15 +88,31 @@ def _sigmoid(x, x0, k, ymin, ymax):
     return ymin + height_diff / (1 + np.exp(-k * (x - x0)))
 
 
-def _median_mah_sigmoid_params(logmpeak):
-    logtc = _logtc_param_model(logmpeak)
-    logtk = np.zeros_like(logtc) + 3.0
-    dlogm_height = np.zeros_like(logtc) + 5.0
+def _median_mah_sigmoid_params(logmpeak, **params):
+    logtc_logm0 = params.get("logtc_logm0", DEFAULT_MAH_PARAMS["logtc_logm0"])
+    logtc_k = params.get("logtc_k", DEFAULT_MAH_PARAMS["logtc_k"])
+    logtc_ymin = params.get("logtc_ymin", DEFAULT_MAH_PARAMS["logtc_ymin"])
+    logtc_ymax = params.get("logtc_ymax", DEFAULT_MAH_PARAMS["logtc_ymax"])
+    logtc = _logtc_param_model(
+        logmpeak,
+        logtc_logm0=logtc_logm0,
+        logtc_k=logtc_k,
+        logtc_ymin=logtc_ymin,
+        logtc_ymax=logtc_ymax,
+    )
+
+    logtk = np.zeros_like(logmpeak) + DEFAULT_MAH_PARAMS["logtk"]
+    dlogm_height = np.zeros_like(logmpeak) + DEFAULT_MAH_PARAMS["dlogm_height"]
+
     return logtc, logtk, dlogm_height
 
 
 def _logtc_param_model(
-    logm, logtc_logm0=8.3, logtc_k=0.4, logtc_ymin=1.635, logtc_ymax=-0.365
+    logm,
+    logtc_logm0=DEFAULT_MAH_PARAMS["logtc_logm0"],
+    logtc_k=DEFAULT_MAH_PARAMS["logtc_k"],
+    logtc_ymin=DEFAULT_MAH_PARAMS["logtc_ymin"],
+    logtc_ymax=DEFAULT_MAH_PARAMS["logtc_ymax"],
 ):
     return _sigmoid(logm, logtc_logm0, logtc_k, logtc_ymin, logtc_ymax)
 
