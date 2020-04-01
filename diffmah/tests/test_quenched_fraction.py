0e164d128a62a49781dac80a6a9b97a4d95a10fd
Andrew Hearin <ahearin@anl.gov>
2020-04-01 12:45:06 -0500

    Added failing unit-test showing buggy behavior with tobs

--- diffmah/tests/test_quenched_fraction.py
+++ diffmah/tests/test_quenched_fraction.py
@@ -15,7 +15,7 @@ def test_qprob_at_tobs_returns_sensible_values():
 def test_qprob_at_tobs_scales_sensibly_with_mass():
     tobs = 13.5
     logmarr = np.linspace(10, 15, 25)
-    qfrac = np.array([qprob_at_tobs(logm, tobs) for logm in logmarr])
+    qfrac = np.array([qprob_at_tobs(logm0, tobs) for logm0 in logmarr])
     dq = np.diff(qfrac)
 
     assert np.all(qfrac >= 0)
@@ -23,3 +23,17 @@ def test_qprob_at_tobs_scales_sensibly_with_mass():
     assert np.all(qfrac <= 1)
     assert np.any(qfrac < 1)
     assert np.all(dq > 0)
+
+
+def test_qprob_at_tobs_scales_sensibly_with_time():
+    logm0 = 12
+    tarr = np.linspace(1, 13.8, 40)
+    qfrac = np.array([qprob_at_tobs(logm0, t) for t in tarr])
+    dq = np.diff(qfrac)
+
+    assert np.all(qfrac >= 0)
+    assert np.any(qfrac > 0)
+    assert np.all(qfrac <= 1)
+    assert np.any(qfrac < 1)
+    assert np.all(dq >= 0)
+    assert not np.allclose(dq, 0)
