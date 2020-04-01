cf73911c911116f9d806f79ea759c06edb49361e
Andrew Hearin <ahearin@anl.gov>
2020-04-01 12:42:56 -0500

    Added unit-test to enforce that qprob_at_tobs is monotonic in mass

--- diffmah/tests/test_quenched_fraction.py
+++ diffmah/tests/test_quenched_fraction.py
@@ -1,8 +1,25 @@
 """
 """
+import numpy as np
 from ..quenched_fraction import qprob_at_tobs
 
 
-def test1():
-    qfrac = qprob_at_tobs(12, 13.5)
-    assert 0 <= qfrac <= 1
+def test_qprob_at_tobs_returns_sensible_values():
+
+    for logm in np.linspace(10, 15, 6):
+        for tobs in np.linspace(0.1, 13.8, 7):
+            qfrac = qprob_at_tobs(12, 13.5)
+            assert 0 <= qfrac <= 1
+
+
+def test_qprob_at_tobs_scales_sensibly_with_mass():
+    tobs = 13.5
+    logmarr = np.linspace(10, 15, 25)
+    qfrac = np.array([qprob_at_tobs(logm, tobs) for logm in logmarr])
+    dq = np.diff(qfrac)
+
+    assert np.all(qfrac >= 0)
+    assert np.any(qfrac > 0)
+    assert np.all(qfrac <= 1)
+    assert np.any(qfrac < 1)
+    assert np.all(dq > 0)
