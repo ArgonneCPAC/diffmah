3f5fd491c233fabb21de316b9eecb44a0ae20673
Andrew Hearin <ahearin@anl.gov>
2020-03-31 19:00:29 -0500

    New unit-test shows new function subsumes behavior of in_situ_mstar_at_zobs. So should now migrate unit-testing

--- diffmah/tests/test_in_situ_history.py
+++ diffmah/tests/test_in_situ_history.py
@@ -1,6 +1,8 @@
 """
 """
+import numpy as np
 from ..in_situ_history import in_situ_galaxy_halo_history
+from ..in_situ_smh_kernel import in_situ_mstar_at_zobs
 
 
 def test_get_model_param_dictionaries():
@@ -8,3 +10,7 @@ def test_get_model_param_dictionaries():
     zarr, tarr, mah, dmd = X[:4]
     sfr_ms_history, sfr_q_history = X[4:6]
     mstar_ms_history, mstar_q_history = X[6:]
+
+    mstar_ms, mstar_q = in_situ_mstar_at_zobs(0, 12)
+    assert np.allclose(mstar_ms_history[-1], mstar_ms, rtol=0.01)
+    assert np.allclose(mstar_q_history[-1], mstar_q, rtol=0.01)
