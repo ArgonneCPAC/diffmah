8d0084d2bbf3ed0352b710e75843263cba508593
Andrew Hearin <ahearin@anl.gov>
2020-04-14 07:57:51 -0500

    Added failing unit-test of newly complete draft of module

--- diffmah/halo_mass_assembly.py
+++ diffmah/halo_mass_assembly.py
@@ -1,12 +1,14 @@
 """
 """
+import numpy as np
 from jax import numpy as jax_np
 from jax import jit as jax_jit
 from jax import vmap as jax_vmap
-import numpy as np
+from jax import grad as jax_grad
 from collections import OrderedDict
+from astropy.cosmology import Planck15
 
-__all__ = ("logmpeak_from_logt",)
+__all__ = ("halo_logmpeak_vs_time",)
 
 
 DEFAULT_MAH_PARAMS = OrderedDict(
@@ -22,15 +24,51 @@ DEFAULT_MAH_PARAMS = OrderedDict(
     logtc_scatter_logmc_k=1,
 )
 LOGT0 = 1.14
+COSMIC_AGE_TODAY = 10 ** LOGT0
 
 
-def _logmpeak_from_logt_jax_kern(logt, params):
+def halo_logmpeak_vs_time(
+    cosmic_time, logm0, t0=COSMIC_AGE_TODAY, mah_percentile=None, **kwargs
+):
+    """Median halo mass growth vs cosmic time.
+
+    Parameters
+    ----------
+    cosmic_time : float or ndarray
+        Age of the Universe in Gyr
+
+    logm0 : float or ndarray
+        Base-10 log of peak halo mass in Msun/h
+
+    t0 : float, optional
+        Present-day age of the Universe in Gyr. Default is 13.8.
+
+    Returns
+    -------
+    logmpeak : ndarray
+        Base-10 log of halo mass in Msun/h
+
+    """
+    cosmic_time, logm0 = _get_1d_arrays(cosmic_time, logm0)
+    logtc, logtk, dlogm_height = _get_mah_sigmoid_params(
+        logm0, mah_percentile=mah_percentile, **kwargs
+    )
+    params = logtc, logtk, dlogm_height, logm0
+
+    logmpeak = np.array(logmpeak_vs_time_jax(cosmic_time, t0, params))
+    return logmpeak
+
+
+def _logmpeak_vs_time_jax_kern(cosmic_time, t0, params):
     """Parametric model for halo mass growth vs cosmic time.
 
     Parameters
     ----------
-    logt : float or ndarray
-        Base-10 log of cosmic time in Gyr
+    cosmic_time : float or ndarray
+        Age of the Universe in Gyr
+
+    t0 : float
+        Present-day age of the Universe in Gyr
 
     params : 5-element sequence
         Parameters described in order below
@@ -49,26 +87,116 @@ def _logmpeak_from_logt_jax_kern(logt, params):
     logmpeak_at_t0 : float or ndarray
         Base-10 log of peak halo mass in Msun/h at the time of halo observation
 
-    logt0 : float or ndarray
-        Base-10 log of the time of halo observation.
-
     Returns
     -------
     logmpeak : ndarray
         Base-10 log of halo mass in Msun/h
 
     """
-    logtc, logtk, dlogm_height, logmpeak_at_logt0, logt0 = params
+    logtc, logtk, dlogm_height, logmpeak_at_logt0 = params
+    logt = jax_np.log10(cosmic_time)
+    logt0 = jax_np.log10(t0)
+
     dlogm = _jax_sigmoid(logt, logtc, logtk, -dlogm_height, 0)
     dlogm_at_logt0 = _jax_sigmoid(logt0, logtc, logtk, -dlogm_height, 0)
     logmpeak = logmpeak_at_logt0 + (dlogm - dlogm_at_logt0)
     return logmpeak
 
 
-logmpeak_from_logt = jax_jit(jax_vmap(_logmpeak_from_logt_jax_kern, in_axes=(0, None)))
-logmpeak_from_logt.__doc__ = _logmpeak_from_logt_jax_kern.__doc__
+logmpeak_vs_time_jax = jax_jit(
+    jax_vmap(_logmpeak_vs_time_jax_kern, in_axes=(0, None, 0))
+)
+logmpeak_vs_time_jax.__doc__ = _logmpeak_vs_time_jax_kern.__doc__
+
+
+def _get_mah_sigmoid_params(logm0, mah_percentile=None, **kwargs):
+    """MAH parameters for a halo with present-day mass logm0.
+    """
+    logm0 = np.atleast_1d(logm0)
+
+    logtc_keys = ("logtc_logm0", "logtc_k", "logtc_ymin", "logtc_ymax")
+    if mah_percentile is None:
+        logtc_params = OrderedDict(
+            [(key, kwargs.get(key, DEFAULT_MAH_PARAMS[key])) for key in logtc_keys]
+        )
+        logtc = np.zeros_like(logm0) + _logtc_param_model(logm0, **logtc_params)
+    else:
+        msg = "Do not pass mah_percentile argument and {0} argument"
+        for key in logtc_keys:
+            assert key not in kwargs, msg.format(key)
+        logtc = np.zeros_like(logm0) + _logtc_from_mah_percentile(
+            logm0, mah_percentile, **kwargs
+        )
+
+    _logtk = kwargs.get("logtk", DEFAULT_MAH_PARAMS["logtk"])
+    logtk = np.zeros_like(logm0) + _logtk
+
+    _dlogm_height = kwargs.get("dlogm_height", DEFAULT_MAH_PARAMS["dlogm_height"])
+    dlogm_height = np.zeros_like(logm0) + _dlogm_height
+
+    return logtc, logtk, dlogm_height
+
+
+def _logtc_from_mah_percentile(logm0, p, **mah_params):
+    logtc_scatter_logmc = mah_params.get(
+        "logtc_scatter_logmc", DEFAULT_MAH_PARAMS["logtc_scatter_logmc"]
+    )
+    logtc_scatter_logmc_k = mah_params.get(
+        "logtc_scatter_logmc_k", DEFAULT_MAH_PARAMS["logtc_scatter_logmc_k"]
+    )
+    logtc_scatter_dwarfs = mah_params.get(
+        "logtc_scatter_dwarfs", DEFAULT_MAH_PARAMS["logtc_scatter_dwarfs"]
+    )
+    logtc_scatter_clusters = mah_params.get(
+        "logtc_scatter_clusters", DEFAULT_MAH_PARAMS["logtc_scatter_clusters"]
+    )
+    logtc_scatter = _jax_sigmoid(
+        logm0,
+        logtc_scatter_logmc,
+        logtc_scatter_logmc_k,
+        logtc_scatter_dwarfs,
+        logtc_scatter_clusters,
+    )
+    logtc_logm0 = mah_params.get("logtc_logm0", DEFAULT_MAH_PARAMS["logtc_logm0"])
+    logtc_k = mah_params.get("logtc_k", DEFAULT_MAH_PARAMS["logtc_k"])
+    logtc_ymin = mah_params.get("logtc_ymin", DEFAULT_MAH_PARAMS["logtc_ymin"])
+    logtc_ymax = mah_params.get("logtc_ymax", DEFAULT_MAH_PARAMS["logtc_ymax"])
+    logtc_med = _logtc_param_model(
+        logm0,
+        logtc_logm0=logtc_logm0,
+        logtc_k=logtc_k,
+        logtc_ymin=logtc_ymin,
+        logtc_ymax=logtc_ymax,
+    )
+    logtc_lo, logtc_hi = logtc_med - logtc_scatter, logtc_med + logtc_scatter
+    logtc = logtc_lo + p * (logtc_hi - logtc_lo)
+    return logtc
+
+
+def _logtc_param_model(
+    logm,
+    logtc_logm0=DEFAULT_MAH_PARAMS["logtc_logm0"],
+    logtc_k=DEFAULT_MAH_PARAMS["logtc_k"],
+    logtc_ymin=DEFAULT_MAH_PARAMS["logtc_ymin"],
+    logtc_ymax=DEFAULT_MAH_PARAMS["logtc_ymax"],
+):
+    return _jax_sigmoid(logm, logtc_logm0, logtc_k, logtc_ymin, logtc_ymax)
 
 
 def _jax_sigmoid(x, x0, k, ymin, ymax):
     height_diff = ymax - ymin
     return ymin + height_diff / (1 + jax_np.exp(-k * (x - x0)))
+
+
+def _get_1d_arrays(*args):
+    """Return a list of ndarrays of the same length.
+
+    Each arg must be either an ndarray of shape (npts, ), or a scalar.
+
+    """
+    results = [np.atleast_1d(arg) for arg in args]
+    sizes = [arr.size for arr in results]
+    npts = max(sizes)
+    msg = "All input arguments should be either a float or ndarray of shape ({0}, )"
+    assert set(sizes) <= set((1, npts)), msg.format(npts)
+    return [np.zeros(npts).astype(arr.dtype) + arr for arr in results]
--- diffmah/tests/test_halo_mass_assembly.py
+++ diffmah/tests/test_halo_mass_assembly.py
@@ -1,20 +1,23 @@
 """
 """
 import numpy as np
-from ..halo_mass_assembly import logmpeak_from_logt as logmpeak_from_logt_jax
+from ..halo_mass_assembly import halo_logmpeak_vs_time
 from ..sigmoid_mah import logmpeak_from_logt as logmpeak_from_logt_np
 
 
 def test_jax_mah_model_agrees_with_numpy_sigmoid_mah():
     logtk, dlogm_height = 3, 5
     logt0 = 1.14
+    t0 = 10 ** logt0
     logtc = 0
 
-    for logm0 in np.linspace(10, 15, 15):
-        logt = np.linspace(-0.5, 1.14, 20)
+    for logm0 in np.linspace(5, 20, 25):
+        time = np.linspace(0.1, t0, 25)
+        logt = np.log10(time)
         logmpeak_np = logmpeak_from_logt_np(
             logt, logtc, logtk, dlogm_height, logm0, logt0
         )
-        params = logtc, logtk, dlogm_height, logm0, logt0
-        logmpeak_jax = logmpeak_from_logt_jax(logt, params)
-        assert np.allclose(logmpeak_jax, logmpeak_np)
+        logmpeak_jax = halo_logmpeak_vs_time(
+            time, logm0, t0=t0, logtc=logtc, logtk=logtk, dlogm_height=dlogm_height,
+        )
+        assert np.allclose(logmpeak_np, logmpeak_jax, rtol=0.001)
