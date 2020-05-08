044880750eb4492e704d7044e0e0df26b4bc526c
Andrew Hearin <ahearin@anl.gov>
2020-05-08 07:18:51 -0500

    Fixed pep8 error in test_log_ms_fraction_um_median1

--- diffmah/tests/test_quenching_history.py
+++ diffmah/tests/test_quenching_history.py
@@ -6,4 +6,4 @@ from ..quenching_history import log_ms_fraction_um_median
 
 def test_log_ms_fraction_um_median1():
     lgtarr = np.linspace(0, 1.15, 500)
-    s = [log_ms_fraction_um_median(lgm, lgtarr) for lgm in np.arange(10, 15, 0.5)]
+    [log_ms_fraction_um_median(lgm, lgtarr) for lgm in np.arange(10, 15, 0.5)]
