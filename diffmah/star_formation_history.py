3febff41a75ebab04c8678edccb89ee2e2dad1d3
Andrew Hearin <ahearin@anl.gov>
2020-05-11 15:45:04 -0500

    Cleaned up star_formation_history.py module

--- diffmah/star_formation_history.py
+++ diffmah/star_formation_history.py
@@ -1,8 +1,6 @@
 """
 """
 from jax import numpy as jax_np
-from jax import jit as jax_jit
-from jax import vmap as jax_vmap
 from .quenching_history import _mean_log_main_sequence_fraction
 from .sfr_efficiency import mean_log_sfr_efficiency_ms_jax
 from .halo_assembly import _mean_halo_assembly_function
@@ -10,7 +8,7 @@ from .halo_assembly import _mean_halo_assembly_function
 FB = 0.158
 
 
-def _mean_log_mstar_history(
+def _mean_log_mstar_history_jax_kern(
     mean_mah_params,
     mean_sfr_eff_params,
     q_params,
@@ -20,7 +18,7 @@ def _mean_log_mstar_history(
     dt,
     indx_pred,
 ):
-    log_sfr_table = _mean_log_sfr_history(
+    log_sfr_table = _mean_log_sfr_history_jax_kern(
         mean_mah_params, mean_sfr_eff_params, q_params, logm0, logt_table, indx_t0
     )
     log_smh_table = (
@@ -29,7 +27,7 @@ def _mean_log_mstar_history(
     return log_sfr_table[indx_pred], log_smh_table[indx_pred]
 
 
-def _mean_log_sfr_history(
+def _mean_log_sfr_history_jax_kern(
     mean_mah_params, mean_sfr_eff_params, q_params, logm0, logt, indx_t0
 ):
     tarr = jax_np.power(10, logt)
@@ -43,8 +41,3 @@ def _mean_log_sfr_history(
     log_frac_ms = _mean_log_main_sequence_fraction(q_params, logm0, logt)
     log_sfr = log_dmbdt + log_sfr_eff_ms + log_frac_ms
     return log_sfr
-
-
-mean_log_sfr_history = jax_jit(
-    jax_vmap(_mean_log_sfr_history, in_axes=(None, None, None, None, 0, None))
-)
--- diffmah/tests/test_star_formation_history.py
+++ diffmah/tests/test_star_formation_history.py
@@ -1,7 +1,8 @@
 """
 """
 import numpy as np
-from ..star_formation_history import _mean_log_sfr_history, _mean_log_mstar_history
+from ..star_formation_history import _mean_log_mstar_history_jax_kern
+from ..star_formation_history import _mean_log_sfr_history_jax_kern
 from ..quenching_history import MEDIAN_HISTORY_PARAMS as MEAN_Q_PARAMS
 from ..sfr_efficiency import MEDIAN_SFR_MS_PARAMS
 from ..halo_assembly import MEAN_MAH_PARAMS
@@ -14,7 +15,7 @@ def test_mean_log_sfr_history():
     logm0 = 12
     logt = np.linspace(0, 1.141, 50)
     indx_t0 = -1
-    log_sfrh = _mean_log_sfr_history(
+    log_sfrh = _mean_log_sfr_history_jax_kern(
         mah_params, mean_sfr_eff_params, q_params, logm0, logt, indx_t0
     )
     assert np.all(np.isfinite(log_sfrh))
@@ -30,7 +31,7 @@ def test_mean_log_mstar_history():
     indx_t0 = -1
     dt = np.diff(tarr)[-1]
     indx_pred = np.array((10, 20, 30)).astype("i4")
-    log_sfrh, log_smh = _mean_log_mstar_history(
+    log_sfrh, log_smh = _mean_log_mstar_history_jax_kern(
         mah_params,
         mean_sfr_eff_params,
         q_params,
