16552426eed20815d1156d517bafd47bc8db24c5
Andrew Hearin <ahearin@anl.gov>
2020-11-08 13:33:08 -0600

    Finished implementation of avg_assembly_history_tmparr function and brought in the fitting functions approximating Consistent Trees growth

--- diffmah/bundle_history_prediction.py
+++ diffmah/bundle_history_prediction.py
@@ -3,9 +3,11 @@ which integrates a Gaussian over the dmhdt_late_index parameter that varies
 the late-time power-law index, alpha, where dmh/dt ~ t**alpha
 """
 import numpy as np
-from diffmah.halo_assembly import _individual_halo_assembly_jax_kern, _get_dt_array
-from diffmah.halo_assembly import DEFAULT_MAH_PARAMS
-from jax import vmap
+from .mean_um_relation_model import _prob_tmp_early_forming_cens, _frac_early_mpeak
+from .mean_um_relation_model import PARAMS as UM_PARAMS
+from .halo_assembly import _individual_halo_assembly_jax_kern, _get_dt_array
+from .halo_assembly import DEFAULT_MAH_PARAMS, TODAY
+from jax import vmap as jvmap
 from jax import numpy as jnp
 from jax import jit as jjit
 from jax.scipy.stats import multivariate_normal as jnorm
@@ -13,19 +15,26 @@ from collections import OrderedDict
 
 
 MEAN_DMHDT_EARLY_INDX_DICT = OrderedDict(
-    mean_dmhdt_early_x0=13,
-    mean_dmhdt_early_k=1,
-    mean_dmhdt_early_ylo=0.2,
-    mean_dmhdt_early_yhi=0.5,
-    mean_dmhdt_early_scatter=0.55,
+    mean_dmhdt_early_x0=13.5,
+    mean_dmhdt_early_k=0.6,
+    mean_dmhdt_early_ylo=0.0,
+    mean_dmhdt_early_yhi=2.4,
+    mean_dmhdt_early_log_scatter=-0.75,
 )
 
 MEAN_DMHDT_LATE_INDX_DICT = OrderedDict(
-    mean_dmhdt_late_x0=13,
-    mean_dmhdt_late_k=1,
-    mean_dmhdt_late_ylo=-1.25,
-    mean_dmhdt_late_yhi=-0.5,
-    mean_dmhdt_late_scatter=1.25,
+    mean_dmhdt_late_x0=14.25,
+    mean_dmhdt_late_k=0.7,
+    mean_dmhdt_late_ylo=-0.15,
+    mean_dmhdt_late_yhi=-0.87,
+    mean_dmhdt_late_log_scatter=-0.3,
+)
+
+MEAN_DMHDT_X0_DICT = OrderedDict(
+    mean_dmhdt_x0_x0=13.91,
+    mean_dmhdt_x0_k=0.48,
+    mean_dmhdt_x0_ylo=-0.46,
+    mean_dmhdt_x0_yhi=1.00,
 )
 
 
@@ -47,12 +56,37 @@ def _halo_assembly(
     return mah, dmhdt
 
 
-_a = (None, None, 0, None, None, None, None, None)
-_f0 = vmap(_halo_assembly, in_axes=_a)
+def _clipped_halo_assembly(
+    logt, dtarr, logmp, dmhdt_x0, dmhdt_k, dmhdt_early_index, dmhdt_late_index, indx_tmp
+):
+    mah, dmhdt = _halo_assembly(
+        logt,
+        dtarr,
+        logmp,
+        dmhdt_x0,
+        dmhdt_k,
+        dmhdt_early_index,
+        dmhdt_late_index,
+        indx_tmp,
+    )
+    mpeak = 10.0 ** logmp
+    msk = mah > mpeak
+    mah = jnp.where(msk, mpeak, mah)
+    dmhdt = jnp.where(msk, 0.0, dmhdt)
+    return mah, dmhdt
+
+
+_a = (None, None, 0, 0, None, None, None, None)
+_f0 = jvmap(_clipped_halo_assembly, in_axes=_a)
 _a = (None, None, None, None, None, None, 0, None)
-_f1 = jjit(vmap(_f0, in_axes=_a))
+_f1 = jjit(jvmap(_f0, in_axes=_a))
 _a = (None, None, None, None, None, 0, None, None)
-_generate_halo_history_bundle_jax = jjit(vmap(_f1, in_axes=_a))
+_generate_halo_history_bundle_jax = jjit(jvmap(_f1, in_axes=_a))
+
+_a = (None, None, None, None, None, None, None, 0)
+_generate_halo_history_bundle_tmparr_jax = jjit(
+    jvmap(_generate_halo_history_bundle_jax, in_axes=_a)
+)
 
 
 def generate_halo_history_bundle(
@@ -132,6 +166,8 @@ def _dmhdt_late_index_pdf(
     mean_late_indx = _mean_dmhdt_late_index(
         logmp, dmhdt_late_x0, dmhdt_late_k, dmhdt_late_ylo, dmhdt_late_yhi
     )
+    msk = dmhdt_late_scatter < 0.01
+    dmhdt_late_scatter = jnp.where(msk, 0.01, dmhdt_late_scatter)
     return jnorm.pdf(late_indx, mean_late_indx, dmhdt_late_scatter)
 
 
@@ -140,6 +176,11 @@ def _mean_dmhdt_late_index(
     lgm, dmhdt_late_x0, dmhdt_late_k, dmhdt_late_ylo, dmhdt_late_yhi
 ):
     return _sigmoid(lgm, dmhdt_late_x0, dmhdt_late_k, dmhdt_late_ylo, dmhdt_late_yhi)
+
+
+@jjit
+def _mean_dmhdt_x0(lgm, dmhdt_x0_x0, dmhdt_x0_k, dmhdt_x0_ylo, dmhdt_x0_yhi):
+    return _sigmoid(lgm, dmhdt_x0_x0, dmhdt_x0_k, dmhdt_x0_ylo, dmhdt_x0_yhi)
 
 
 @jjit
@@ -155,6 +196,8 @@ def _dmhdt_early_index_pdf(
     mean_early_indx = _mean_dmhdt_early_index(
         logmp, dmhdt_early_x0, dmhdt_early_k, dmhdt_early_ylo, dmhdt_early_yhi
     )
+    msk = dmhdt_early_scatter < 0.01
+    dmhdt_early_scatter = jnp.where(msk, 0.01, dmhdt_early_scatter)
     return jnorm.pdf(early_indx, mean_early_indx, dmhdt_early_scatter)
 
 
@@ -211,19 +254,19 @@ def _sigmoid(x, x0, k, ymin, ymax):
     return ymin + height_diff / (1 + jnp.exp(-k * (x - x0)))
 
 
-_f = vmap(_dmhdt_late_index_pdf, in_axes=(0, None, None, None, None, None, None))
+_f = jvmap(_dmhdt_late_index_pdf, in_axes=(0, None, None, None, None, None, None))
 _get_late_indx_weights_jax = jjit(
-    vmap(_f, in_axes=(None, 0, None, None, None, None, None))
+    jvmap(_f, in_axes=(None, 0, None, None, None, None, None))
 )
 
-_f = vmap(_dmhdt_early_index_pdf, in_axes=(0, None, None, None, None, None, None))
+_f = jvmap(_dmhdt_early_index_pdf, in_axes=(0, None, None, None, None, None, None))
 _get_early_indx_weights_jax = jjit(
-    vmap(_f, in_axes=(None, 0, None, None, None, None, None))
+    jvmap(_f, in_axes=(None, 0, None, None, None, None, None))
 )
 
-_f = vmap(_dmhdt_index_pdf, in_axes=(0, *[None] * 12))
-_g = vmap(_f, in_axes=(None, None, 0, *[None] * 10))
-_get_indx_weights_jax = vmap(_g, in_axes=(None, 0, *[None] * 11))
+_f = jvmap(_dmhdt_index_pdf, in_axes=(0, *[None] * 12))
+_g = jvmap(_f, in_axes=(None, None, 0, *[None] * 10))
+_get_indx_weights_jax = jvmap(_g, in_axes=(None, 0, *[None] * 11))
 
 
 @jjit
@@ -355,7 +398,8 @@ def get_dmhdt_late_index_weight_bundle(
     mean_dmhdt_late_k=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_k"],
     mean_dmhdt_late_ylo=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_ylo"],
     mean_dmhdt_late_yhi=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_yhi"],
-    mean_dmhdt_late_scatter=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_scatter"],
+    mean_dmhdt_late_scatter=10
+    ** MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_log_scatter"],
 ):
     """
     Calculate the weights to assign to each halo when summing over the bundle to
@@ -410,81 +454,29 @@ def get_dmhdt_late_index_weight_bundle(
     return np.array(weight_bundle)
 
 
-@jjit
-def _avg_assembly_history(
-    logt,
-    dtarr,
-    logmp_grid,
-    dmhdt_x0,
-    dmhdt_k,
-    dmhdt_early_index_grid,
-    dmhdt_late_index_grid,
-    indx_tmp,
-    mean_dmhdt_early_x0,
-    mean_dmhdt_early_k,
-    mean_dmhdt_early_ylo,
-    mean_dmhdt_early_yhi,
-    mean_dmhdt_early_scatter,
-    mean_dmhdt_late_x0,
-    mean_dmhdt_late_k,
-    mean_dmhdt_late_ylo,
-    mean_dmhdt_late_yhi,
-    mean_dmhdt_late_scatter,
-):
-    bundle_generator_args = (
-        logt,
-        dtarr,
-        logmp_grid,
-        dmhdt_x0,
-        dmhdt_k,
-        dmhdt_early_index_grid,
-        dmhdt_late_index_grid,
-        indx_tmp,
-    )
-    mah_bundle, dmhdt_bundle = _generate_halo_history_bundle_jax(*bundle_generator_args)
-
-    indx_weights = _get_indx_weight_bundle(
-        logmp_grid,
-        dmhdt_early_index_grid,
-        dmhdt_late_index_grid,
-        mean_dmhdt_early_x0,
-        mean_dmhdt_early_k,
-        mean_dmhdt_early_ylo,
-        mean_dmhdt_early_yhi,
-        mean_dmhdt_early_scatter,
-        mean_dmhdt_late_x0,
-        mean_dmhdt_late_k,
-        mean_dmhdt_late_ylo,
-        mean_dmhdt_late_yhi,
-        mean_dmhdt_late_scatter,
-    )
-    n_early = dmhdt_early_index_grid.size
-    n_late = dmhdt_late_index_grid.size
-    n_mass = logmp_grid.size
-    _W = indx_weights.reshape(n_early, n_late, n_mass, 1)
-    avg_log_mah = jnp.log10(jnp.sum(jnp.sum(mah_bundle * _W, axis=0), axis=0))
-    avg_log_dmhdt = jnp.log10(jnp.sum(jnp.sum(dmhdt_bundle * _W, axis=0), axis=0))
-    return avg_log_mah, avg_log_dmhdt
-
-
 def average_halo_assembly_history(
     cosmic_time,
     logmp_grid,
     dmhdt_early_index_grid,
     dmhdt_late_index_grid,
-    dmhdt_x0=DEFAULT_MAH_PARAMS["dmhdt_x0"],
     dmhdt_k=DEFAULT_MAH_PARAMS["dmhdt_k"],
     tmp=None,
+    mean_dmhdt_x0_x0=MEAN_DMHDT_X0_DICT["mean_dmhdt_x0_x0"],
+    mean_dmhdt_x0_k=MEAN_DMHDT_X0_DICT["mean_dmhdt_x0_k"],
+    mean_dmhdt_x0_ylo=MEAN_DMHDT_X0_DICT["mean_dmhdt_x0_ylo"],
+    mean_dmhdt_x0_yhi=MEAN_DMHDT_X0_DICT["mean_dmhdt_x0_yhi"],
     mean_dmhdt_early_x0=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_x0"],
     mean_dmhdt_early_k=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_k"],
     mean_dmhdt_early_ylo=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_ylo"],
     mean_dmhdt_early_yhi=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_yhi"],
-    mean_dmhdt_early_scatter=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_scatter"],
+    mean_dmhdt_early_scatter=10
+    ** MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_log_scatter"],
     mean_dmhdt_late_x0=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_x0"],
     mean_dmhdt_late_k=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_k"],
     mean_dmhdt_late_ylo=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_ylo"],
     mean_dmhdt_late_yhi=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_yhi"],
-    mean_dmhdt_late_scatter=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_scatter"],
+    mean_dmhdt_late_scatter=10
+    ** MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_log_scatter"],
 ):
     """
     Calculate mean halo history, averaging over Gaussian-distributed dmhdt_late_index.
@@ -572,11 +564,14 @@ def average_halo_assembly_history(
         logt,
         dtarr,
         logmp_grid,
-        dmhdt_x0,
         dmhdt_k,
         dmhdt_early_index_grid,
         dmhdt_late_index_grid,
         indx_tmp,
+        mean_dmhdt_x0_x0,
+        mean_dmhdt_x0_k,
+        mean_dmhdt_x0_ylo,
+        mean_dmhdt_x0_yhi,
         mean_dmhdt_early_x0,
         mean_dmhdt_early_k,
         mean_dmhdt_early_ylo,
@@ -589,3 +584,277 @@ def average_halo_assembly_history(
         mean_dmhdt_late_scatter,
     )
     return np.array(avg_log_mah), np.array(avg_log_dmhdt)
+
+
+@jjit
+def _avg_assembly_history(
+    logt,
+    dtarr,
+    logmp_grid,
+    dmhdt_k,
+    dmhdt_early_index_grid,
+    dmhdt_late_index_grid,
+    indx_tmp,
+    mean_dmhdt_x0_x0,
+    mean_dmhdt_x0_k,
+    mean_dmhdt_x0_ylo,
+    mean_dmhdt_x0_yhi,
+    mean_dmhdt_early_x0,
+    mean_dmhdt_early_k,
+    mean_dmhdt_early_ylo,
+    mean_dmhdt_early_yhi,
+    mean_dmhdt_early_log_scatter,
+    mean_dmhdt_late_x0,
+    mean_dmhdt_late_k,
+    mean_dmhdt_late_ylo,
+    mean_dmhdt_late_yhi,
+    mean_dmhdt_late_log_scatter,
+):
+    mean_dmhdt_early_scatter = 10 ** mean_dmhdt_early_log_scatter
+    mean_dmhdt_late_scatter = 10 ** mean_dmhdt_late_log_scatter
+
+    dmhdt_x0_grid = _mean_dmhdt_x0(
+        logmp_grid,
+        mean_dmhdt_x0_x0,
+        mean_dmhdt_x0_k,
+        mean_dmhdt_x0_ylo,
+        mean_dmhdt_x0_yhi,
+    )
+    bundle_generator_args = (
+        logt,
+        dtarr,
+        logmp_grid,
+        dmhdt_x0_grid,
+        dmhdt_k,
+        dmhdt_early_index_grid,
+        dmhdt_late_index_grid,
+        indx_tmp,
+    )
+    mah_bundle, dmhdt_bundle = _generate_halo_history_bundle_jax(*bundle_generator_args)
+
+    indx_weights = _get_indx_weight_bundle(
+        logmp_grid,
+        dmhdt_early_index_grid,
+        dmhdt_late_index_grid,
+        mean_dmhdt_early_x0,
+        mean_dmhdt_early_k,
+        mean_dmhdt_early_ylo,
+        mean_dmhdt_early_yhi,
+        mean_dmhdt_early_scatter,
+        mean_dmhdt_late_x0,
+        mean_dmhdt_late_k,
+        mean_dmhdt_late_ylo,
+        mean_dmhdt_late_yhi,
+        mean_dmhdt_late_scatter,
+    )
+    n_early = dmhdt_early_index_grid.size
+    n_late = dmhdt_late_index_grid.size
+    n_mass = logmp_grid.size
+    _W = indx_weights.reshape(n_early, n_late, n_mass, 1)
+    avg_log_mah = jnp.log10(jnp.sum(jnp.sum(mah_bundle * _W, axis=0), axis=0))
+    avg_log_dmhdt = jnp.log10(jnp.sum(jnp.sum(dmhdt_bundle * _W, axis=0), axis=0))
+    return avg_log_mah, avg_log_dmhdt
+
+
+_prob_tmp_grid = jvmap(
+    _prob_tmp_early_forming_cens, in_axes=(0, None, None, None, None)
+)
+
+
+@jjit
+def _get_tmp_weight_bundle(logmp_grid, tmp_grid, t0, tmp_k, tmp_indx_t0):
+    _tmp_weights = _prob_tmp_grid(logmp_grid, tmp_grid, t0, tmp_k, tmp_indx_t0).T
+    tmp_weight_norms = jnp.sum(_tmp_weights, axis=0)
+    tmp_weights = _tmp_weights / tmp_weight_norms
+    return tmp_weights
+
+
+@jjit
+def _avg_assembly_history_tmparr(
+    logt,
+    dtarr,
+    logmp_grid,
+    dmhdt_k,
+    dmhdt_early_index_grid,
+    dmhdt_late_index_grid,
+    indx_tmp_grid,
+    tmp_grid,
+    today,
+    mean_dmhdt_x0_x0,
+    mean_dmhdt_x0_k,
+    mean_dmhdt_x0_ylo,
+    mean_dmhdt_x0_yhi,
+    mean_dmhdt_early_x0,
+    mean_dmhdt_early_k,
+    mean_dmhdt_early_ylo,
+    mean_dmhdt_early_yhi,
+    mean_dmhdt_early_log_scatter,
+    mean_dmhdt_late_x0,
+    mean_dmhdt_late_k,
+    mean_dmhdt_late_ylo,
+    mean_dmhdt_late_yhi,
+    mean_dmhdt_late_log_scatter,
+    tmp_k=UM_PARAMS["tmp_k"],
+    tmp_indx_t0=UM_PARAMS["tmp_indx_t0"],
+):
+    mean_dmhdt_early_scatter = 10 ** mean_dmhdt_early_log_scatter
+    mean_dmhdt_late_scatter = 10 ** mean_dmhdt_late_log_scatter
+
+    dmhdt_x0_grid = _mean_dmhdt_x0(
+        logmp_grid,
+        mean_dmhdt_x0_x0,
+        mean_dmhdt_x0_k,
+        mean_dmhdt_x0_ylo,
+        mean_dmhdt_x0_yhi,
+    )
+    args = (
+        logt,
+        dtarr,
+        logmp_grid,
+        dmhdt_x0_grid,
+        dmhdt_k,
+        dmhdt_early_index_grid,
+        dmhdt_late_index_grid,
+        indx_tmp_grid,
+    )
+    mah_bundle_tmparr, dmhdt_bundle_tmparr = _generate_halo_history_bundle_tmparr_jax(
+        *args
+    )
+
+    args = (
+        logt,
+        dtarr,
+        logmp_grid,
+        dmhdt_x0_grid,
+        dmhdt_k,
+        dmhdt_early_index_grid,
+        dmhdt_late_index_grid,
+        -1,
+    )
+    mah_bundle_t0, dmhdt_bundle_t0 = _generate_halo_history_bundle_jax(*args)
+
+    indx_weights = _get_indx_weight_bundle(
+        logmp_grid,
+        dmhdt_early_index_grid,
+        dmhdt_late_index_grid,
+        mean_dmhdt_early_x0,
+        mean_dmhdt_early_k,
+        mean_dmhdt_early_ylo,
+        mean_dmhdt_early_yhi,
+        mean_dmhdt_early_scatter,
+        mean_dmhdt_late_x0,
+        mean_dmhdt_late_k,
+        mean_dmhdt_late_ylo,
+        mean_dmhdt_late_yhi,
+        mean_dmhdt_late_scatter,
+    )
+    n_early = dmhdt_early_index_grid.size
+    n_late = dmhdt_late_index_grid.size
+    n_mass = logmp_grid.size
+    n_times = logt.size
+
+    tmp_weights = _get_tmp_weight_bundle(
+        logmp_grid, tmp_grid, today, tmp_k, tmp_indx_t0
+    )
+    n_tmp = tmp_weights.shape[0]
+
+    _W = indx_weights.reshape(1, n_early, n_late, n_mass, 1)
+    _W2 = tmp_weights.reshape((n_tmp, 1, 1, n_mass, 1))
+    w_tmparr = _W * _W2
+    avg_mah_tmparr = jnp.sum(mah_bundle_tmparr * w_tmparr, axis=(0, 1, 2))
+    avg_dmhdt_tmparr = jnp.sum(dmhdt_bundle_tmparr * w_tmparr, axis=(0, 1, 2))
+
+    avg_log_mah_tmparr = jnp.log10(avg_mah_tmparr)
+    log_mah_bundle_tmparr = jnp.log10(mah_bundle_tmparr)
+    s = (1, 1, 1, n_mass, n_times)
+    delta_log_mah_tmparr = log_mah_bundle_tmparr - avg_log_mah_tmparr.reshape(s)
+    msk = jnp.isfinite(delta_log_mah_tmparr)
+    delta_log_mah_tmparr = jnp.where(msk, delta_log_mah_tmparr, 0.0)
+    std_mah_tmparr = jnp.sqrt(
+        jnp.sum(w_tmparr * delta_log_mah_tmparr ** 2, axis=(0, 1, 2))
+    )
+
+    w_t0 = indx_weights.reshape(n_early, n_late, n_mass, 1)
+    avg_mah_t0 = jnp.sum(mah_bundle_t0 * w_t0, axis=(0, 1))
+    avg_dmhdt_t0 = jnp.sum(dmhdt_bundle_t0 * w_t0, axis=(0, 1))
+
+    avg_log_mah_t0 = jnp.log10(avg_mah_t0)
+    log_mah_bundle_t0 = jnp.log10(mah_bundle_t0)
+    s = (1, 1, n_mass, n_times)
+    delta_log_mah_t0 = log_mah_bundle_t0 - avg_log_mah_t0.reshape(s)
+    msk = jnp.isfinite(delta_log_mah_t0)
+    delta_log_mah_t0 = jnp.where(msk, delta_log_mah_t0, 0.0)
+    std_mah_t0 = jnp.sqrt(jnp.sum(w_t0 * delta_log_mah_t0 ** 2, axis=(0, 1)))
+
+    f_e = _frac_early_mpeak(logmp_grid).reshape((n_mass, 1))
+    avg_mah = f_e * avg_mah_tmparr + (1 - f_e) * avg_mah_t0
+    avg_dmhdt = f_e * avg_dmhdt_tmparr + (1 - f_e) * avg_dmhdt_t0
+    avg_log_mah = jnp.log10(avg_mah)
+    avg_log_dmhdt = jnp.log10(avg_dmhdt)
+    std_log_mah = f_e * std_mah_tmparr + (1 - f_e) * std_mah_t0
+
+    return avg_log_mah, avg_log_dmhdt, std_log_mah
+
+
+def avg_assembly_history_tmparr(
+    time_grid,
+    logmp_grid,
+    dmhdt_early_index_grid,
+    dmhdt_late_index_grid,
+    indx_tmp_grid,
+    tmp_grid,
+    today=TODAY,
+    dmhdt_k=DEFAULT_MAH_PARAMS["dmhdt_k"],
+    mean_dmhdt_x0_x0=MEAN_DMHDT_X0_DICT["mean_dmhdt_x0_x0"],
+    mean_dmhdt_x0_k=MEAN_DMHDT_X0_DICT["mean_dmhdt_x0_k"],
+    mean_dmhdt_x0_ylo=MEAN_DMHDT_X0_DICT["mean_dmhdt_x0_ylo"],
+    mean_dmhdt_x0_yhi=MEAN_DMHDT_X0_DICT["mean_dmhdt_x0_yhi"],
+    mean_dmhdt_early_x0=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_x0"],
+    mean_dmhdt_early_k=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_k"],
+    mean_dmhdt_early_ylo=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_ylo"],
+    mean_dmhdt_early_yhi=MEAN_DMHDT_EARLY_INDX_DICT["mean_dmhdt_early_yhi"],
+    mean_dmhdt_early_log_scatter=MEAN_DMHDT_EARLY_INDX_DICT[
+        "mean_dmhdt_early_log_scatter"
+    ],
+    mean_dmhdt_late_x0=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_x0"],
+    mean_dmhdt_late_k=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_k"],
+    mean_dmhdt_late_ylo=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_ylo"],
+    mean_dmhdt_late_yhi=MEAN_DMHDT_LATE_INDX_DICT["mean_dmhdt_late_yhi"],
+    mean_dmhdt_late_log_scatter=MEAN_DMHDT_LATE_INDX_DICT[
+        "mean_dmhdt_late_log_scatter"
+    ],
+    tmp_k=UM_PARAMS["tmp_k"],
+    tmp_indx_t0=UM_PARAMS["tmp_indx_t0"],
+):
+    logt = np.log10(time_grid)
+    dtarr = _get_dt_array(time_grid)
+
+    ret = _avg_assembly_history_tmparr(
+        logt,
+        dtarr,
+        logmp_grid,
+        dmhdt_k,
+        dmhdt_early_index_grid,
+        dmhdt_late_index_grid,
+        indx_tmp_grid,
+        tmp_grid,
+        today,
+        mean_dmhdt_x0_x0,
+        mean_dmhdt_x0_k,
+        mean_dmhdt_x0_ylo,
+        mean_dmhdt_x0_yhi,
+        mean_dmhdt_early_x0,
+        mean_dmhdt_early_k,
+        mean_dmhdt_early_ylo,
+        mean_dmhdt_early_yhi,
+        mean_dmhdt_early_log_scatter,
+        mean_dmhdt_late_x0,
+        mean_dmhdt_late_k,
+        mean_dmhdt_late_ylo,
+        mean_dmhdt_late_yhi,
+        mean_dmhdt_late_log_scatter,
+        tmp_k=tmp_k,
+        tmp_indx_t0=tmp_indx_t0,
+    )
+    avg_log_mah, avg_log_dmhdt, std_log_mah = ret
+    return np.array(avg_log_mah), np.array(avg_log_dmhdt), np.array(std_log_mah)
