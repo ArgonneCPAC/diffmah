8f518af21e404fa0ae1112ade5c5a8b1a7c0c192
Andrew Hearin <ahearin@anl.gov>
2021-03-03 13:32:51 -0600

    Added _get_u_x0 convenience function

--- diffmah/individual_halo_assembly.py
+++ diffmah/individual_halo_assembly.py
@@ -53,7 +53,7 @@ def _u_rolling_plaw_vs_logt(logt, logtmp, logmp, u_x0, u_k, u_early, u_dy):
 
 @jjit
 def _get_params_from_u_params(u_x0, u_k, u_early, u_dy):
-    x0 = _sigmoid(u_x0, _PBOUND_X0, _PBOUND_K, *_MAH_BOUNDS["mah_x0"])
+    x0 = _get_x0(u_x0)
     k = _sigmoid(u_k, _PBOUND_X0, _PBOUND_K, *_MAH_BOUNDS["mah_k"])
     early = _get_early_index(u_early)
     late = _get_late_index(u_dy, early)
@@ -62,7 +62,7 @@ def _get_params_from_u_params(u_x0, u_k, u_early, u_dy):
 
 @jjit
 def _get_u_params_from_params(x0, k, early, late):
-    u_x0 = _inverse_sigmoid(x0, _PBOUND_X0, _PBOUND_K, *_MAH_BOUNDS["mah_x0"])
+    u_x0 = _get_u_x0(x0)
     u_k = _inverse_sigmoid(k, _PBOUND_X0, _PBOUND_K, *_MAH_BOUNDS["mah_k"])
     u_early = _get_u_early_index(early)
     u_dy = _get_u_late_index(late, early)
@@ -77,6 +77,16 @@ def _get_late_index(u_dy, early):
 @jjit
 def _get_u_late_index(late, early):
     return _inverse_sigmoid(late, _PBOUND_X0, _PBOUND_K, 0.0, early)
+
+
+@jjit
+def _get_u_x0(x0):
+    return _inverse_sigmoid(x0, _PBOUND_X0, _PBOUND_K, *_MAH_BOUNDS["mah_x0"])
+
+
+@jjit
+def _get_x0(u_x0):
+    return _sigmoid(u_x0, _PBOUND_X0, _PBOUND_K, *_MAH_BOUNDS["mah_x0"])
 
 
 @jjit
