4a72987ee4367f695921f19d0a77c0a84b603a11
Andrew Hearin <ahearin@anl.gov>
2021-03-09 13:57:53 -0600

    Added history_fitting_script.py module

--- scripts/history_fitting_script.py
+++ scripts/history_fitting_script.py
@@ -4,10 +4,21 @@ import os
 from mpi4py import MPI
 import argparse
 from time import time
-from load_um_sfh_data import load_mdpl2_data, load_bpl_data
-from fit_mah import get_loss_data_fixed_logmp, get_loss_data_fixed_k_x0
-from fit_mah import mse_loss_fixed_logmp, mse_loss_fixed_k_x0
-from fit_mah import get_outline_fixed_logmp, get_outline_fixed_k_x0
+from load_mah_data import (
+    load_tng_data,
+    TASSO,
+    BEBOP,
+    load_bolshoi_data,
+    load_mdpl2_data,
+)
+
+from diffmah.fit_mah_helpers import _get_header, get_outline_bad_fit
+from diffmah.fit_mah_helpers import get_loss_data_variable_mp_x0
+from diffmah.fit_mah_helpers import get_loss_data_variable_mp, get_loss_data_fixed_mp
+from diffmah.fit_mah_helpers import mse_loss_variable_mp_x0, mse_loss_fixed_mp
+from diffmah.fit_mah_helpers import mse_loss_variable_mp
+from diffmah.fit_mah_helpers import get_outline_variable_mp_x0, get_outline_fixed_mp
+from diffmah.fit_mah_helpers import get_outline_variable_mp
 from diffmah.utils import jax_adam_wrapper
 import subprocess
 import h5py
@@ -27,75 +38,107 @@ def _write_collated_data(outname, data):
                 hdf[name] = data[:, i]
 
 
-def _get_header():
-    return (
-        "# halo_id logmp dmhdt_x0 dmhdt_k dmhdt_early_index dmhdt_late_index tmp loss\n"
-    )
-
-
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
     rank, nranks = comm.Get_rank(), comm.Get_size()
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("simulation", choices=("bpl", "mdpl"))
-    parser.add_argument("indir", help="Input directory")
-    parser.add_argument("outdir", help="Output directory")
-    parser.add_argument("outbase", help="Basename of the output file")
     parser.add_argument(
-        "modelname",
-        help="Version of the model and loss",
-        choices=("fixed_logmp", "fixed_k_x0"),
+        "simulation", help="name of the simulation (used to select the data loader)"
     )
-    parser.add_argument("-nstep", help="Num opt steps per halo", type=int, default=200)
+    parser.add_argument(
+        "fit_type",
+        help="Defines the loss function",
+        choices=("variable_mp_x0", "fixed_mp", "variable_mp"),
+    )
+    parser.add_argument("outdir", help="Output directory")
+    parser.add_argument("outbase", help="Basename of the output hdf5 file")
+    parser.add_argument("-indir", help="Input directory", default="TASSO")
+    parser.add_argument("-nstep", help="Num opt steps per halo", type=int, default=300)
     parser.add_argument("-test", help="Short test run?", type=bool, default=False)
+    parser.add_argument(
+        "-gal_type",
+        help="Galaxy type (only relevant for Bolshoi and MDPl2)",
+        default="cens",
+    )
     args = parser.parse_args()
 
     start = time()
 
     args = parser.parse_args()
-    rank_outname = os.path.join(args.outdir, TMP_OUTPAT).format(rank)
+    rank_basepat = args.outbase + TMP_OUTPAT
+    rank_outname = os.path.join(args.outdir, rank_basepat).format(rank)
     nstep = args.nstep
 
-    if args.simulation == "bpl":
-        all_halos, tarr, zarr = load_bpl_data(args.indir)
+    fit_type = args.fit_type
+    if fit_type == "variable_mp_x0":
+        mse_loss = mse_loss_variable_mp_x0
+        get_loss_data = get_loss_data_variable_mp_x0
+        get_outline = get_outline_variable_mp_x0
+    elif fit_type == "fixed_mp":
+        mse_loss = mse_loss_fixed_mp
+        get_loss_data = get_loss_data_fixed_mp
+        get_outline = get_outline_fixed_mp
+    elif fit_type == "variable_mp":
+        mse_loss = mse_loss_variable_mp
+        get_loss_data = get_loss_data_variable_mp
+        get_outline = get_outline_variable_mp
+
+    if args.indir == "TASSO":
+        indir = TASSO
+    elif args.indir == "BEBOP":
+        indir = BEBOP
+    else:
+        indir = args.indir
+
+    if args.simulation == "tng":
+        _mah_data = load_tng_data(data_drn=indir)
+        halo_ids, log_mahs, tmpeaks, tarr, lgm_min = _mah_data
+    elif args.simulation == "bpl":
+        _mah_data = load_bolshoi_data(args.gal_type, data_drn=indir)
+        halo_ids, log_mahs, tmpeaks, tarr, lgm_min = _mah_data
     elif args.simulation == "mdpl":
-        all_halos, tarr, zarr = load_mdpl2_data(args.indir)
+        _mah_data = load_mdpl2_data(args.gal_type, data_drn=indir)
+        halo_ids, log_mahs, tmpeaks, tarr, lgm_min = _mah_data
+    else:
+        raise NotImplementedError("Your data loader here")
 
     # Get data for rank
     if args.test:
-        nhalos_tot = nranks * 2
+        nhalos_tot = nranks * 5
     else:
-        nhalos_tot = len(all_halos)
+        nhalos_tot = len(halo_ids)
     _a = np.arange(0, nhalos_tot).astype("i8")
     indx = np.array_split(_a, nranks)[rank]
-    halos_for_rank = all_halos[indx]
-    nhalos_for_rank = len(halos_for_rank)
 
-    # Define the loss function, which params are varied, etc.
-    if args.modelname == "fixed_logmp":
-        get_loss_data = get_loss_data_fixed_logmp
-        loss_func = mse_loss_fixed_logmp
-        get_outline = get_outline_fixed_logmp
-    elif args.modelname == "fixed_k_x0":
-        get_loss_data = get_loss_data_fixed_k_x0
-        loss_func = mse_loss_fixed_k_x0
-        get_outline = get_outline_fixed_k_x0
+    halo_ids_for_rank = halo_ids[indx]
+    log_mahs_for_rank = log_mahs[indx]
+    tmpeaks_for_rank = tmpeaks[indx]
+    nhalos_for_rank = len(halo_ids_for_rank)
 
     header = _get_header()
     with open(rank_outname, "w") as fout:
         fout.write(header)
 
         for i in range(nhalos_for_rank):
-            halo_id = halos_for_rank["halo_id"][i]
-            lgmah = halos_for_rank["log_mah"][i, :]
-            lgdmhdt = halos_for_rank["log_dmhdt"][i, :]
-            tmp = halos_for_rank["tmp"][i]
+            halo_id = halo_ids_for_rank[i]
+            lgmah = log_mahs_for_rank[i, :]
+            tmp = tmpeaks_for_rank[i]
 
-            loss_data, p_init = get_loss_data(tarr, lgmah, lgdmhdt, tmp)
-            fit_data = jax_adam_wrapper(loss_func, p_init, loss_data, nstep)
-            outline = get_outline(halo_id, tmp, loss_data, fit_data)
+            p_init, loss_data = get_loss_data(
+                tarr,
+                lgmah,
+                tmp,
+                lgm_min,
+            )
+            _res = jax_adam_wrapper(mse_loss, p_init, loss_data, nstep, n_warmup=2)
+            p_best, loss_best, loss_arr, params_arr, fit_terminates = _res
+
+            if fit_terminates == 1:
+                outline = get_outline(halo_id, loss_data, p_best, loss_best)
+            else:
+                outline = get_outline_bad_fit(halo_id, lgmah[-1], tmp)
 
             fout.write(outline)
 
@@ -110,7 +153,7 @@ if __name__ == "__main__":
         print(msg.format(nhalos_tot, nranks, runtime))
 
         #  collate data from ranks and rewrite to disk
-        pat = os.path.join(args.outdir, TMP_OUTPAT)
+        pat = os.path.join(args.outdir, rank_basepat)
         fit_data_fnames = [pat.format(i) for i in range(nranks)]
         data_collection = [np.loadtxt(fn) for fn in fit_data_fnames]
         all_fit_data = np.concatenate(data_collection)
