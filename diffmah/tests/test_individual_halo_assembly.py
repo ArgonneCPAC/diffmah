ef6a3234f8188aa142ff4768b71a38f5879bdac3
Andrew Hearin <ahearin@anl.gov>
2021-04-11 17:41:25 -0500

    Added trivial unit test

--- diffmah/tests/test_individual_halo_assembly.py
+++ diffmah/tests/test_individual_halo_assembly.py
@@ -1,6 +1,14 @@
 """
 """
+import numpy as np
+from ..individual_halo_assembly import _calc_halo_history, DEFAULT_MAH_PARAMS
 
 
-def test_something():
-    raise NotImplementedError()
+def test_something_evaluates():
+    tarr = np.linspace(0.1, 14, 500)
+    logt = np.log10(tarr)
+    logtmp = logt[-1]
+    logmp = 12.0
+    dmhdt, log_mah = _calc_halo_history(
+        logt, logtmp, logmp, *DEFAULT_MAH_PARAMS.values()
+    )
