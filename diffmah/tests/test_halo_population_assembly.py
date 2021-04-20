1362309d9c5559df34449f18f76e7c226d2c1cd7
Andrew Hearin <ahearin@anl.gov>
2021-04-20 15:48:23 -0500

    Added unit test

--- diffmah/tests/test_halo_population_assembly.py
+++ diffmah/tests/test_halo_population_assembly.py
@@ -1,7 +1,12 @@
 """
 """
+import os
 import numpy as np
 from ..halo_population_assembly import _get_bimodal_halo_history
+from ..halo_population_assembly import LGE_ARR, LGL_ARR, X0_ARR
+
+_THIS_DRNAME = os.path.dirname(os.path.abspath(__file__))
+DDRN = os.path.join(os.path.dirname(_THIS_DRNAME), "data")
 
 
 def test_get_average_halo_histories():
@@ -28,3 +33,64 @@ def test_get_average_halo_histories():
 
     # Average halo accretion rates should monotonically increase with present-day mass
     assert np.all(np.diff(mean_dmhdt[:, -1]) > 0)
+
+
+def test_average_halo_histories_agree_with_nbody_simulations():
+    mlist = list(
+        (
+            "11.75",
+            "12.00",
+            "12.25",
+            "12.50",
+            "12.75",
+            "13.00",
+            "13.25",
+            "13.50",
+            "13.75",
+            "14.00",
+            "14.25",
+            "14.50",
+            "14.75",
+        )
+    )
+    lgmp_targets = np.array([float(lgm) for lgm in mlist])
+    lgt = np.log10(np.load(os.path.join(DDRN, "nbody_t_target.npy")))
+    mah_pat = "mean_log_mah_nbody_logmp_{}.npy"
+    lgmah_fnames = list((os.path.join(DDRN, mah_pat.format(lgm)) for lgm in mlist))
+    mean_log_mah_targets = np.array([np.load(fn) for fn in lgmah_fnames])
+
+    vmah_pat = "var_log_mah_nbody_logmp_{}.npy"
+    vlgmah_fnames = list((os.path.join(DDRN, vmah_pat.format(lgm)) for lgm in mlist))
+    var_log_mah_targets = np.array([np.load(fn) for fn in vlgmah_fnames])
+
+    dmhdt_pat = "mean_dmhdt_nbody_logmp_{}.npy"
+    dmhdt_fnames = list((os.path.join(DDRN, dmhdt_pat.format(lgm)) for lgm in mlist))
+    mean_dmhdt_targets = np.array([np.load(fn) for fn in dmhdt_fnames])
+
+    vdmhdt_pat = "var_dmhdt_nbody_logmp_{}.npy"
+    vdmhdt_fnames = list((os.path.join(DDRN, vdmhdt_pat.format(lgm)) for lgm in mlist))
+    var_dmhdt_targets = np.array([np.load(fn) for fn in vdmhdt_fnames])
+
+    _res = _get_bimodal_halo_history(lgt, lgmp_targets, LGE_ARR, LGL_ARR, X0_ARR)
+    mean_dmhdt_preds, mean_log_mah_preds = _res[0], _res[2]
+    var_dmhdt_preds, var_log_mah_preds = _res[3], _res[4]
+
+    for im, lgmp in enumerate(lgmp_targets):
+        x, y = mean_log_mah_targets[im, :], mean_log_mah_preds[im, :]
+        msg = "Inaccurate prediction for <log10(MAH)> at lgmp = {0:.2f}"
+        assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
+
+    for im, lgmp in enumerate(lgmp_targets):
+        x, y = np.log10(mean_dmhdt_targets[im, :]), np.log10(mean_dmhdt_preds[im, :])
+        msg = "Inaccurate prediction for <dMh/dt> at lgmp = {0:.2f}"
+        assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
+
+    for im, lgmp in enumerate(lgmp_targets):
+        x, y = var_log_mah_targets[im, :], var_log_mah_preds[im, :]
+        msg = "Inaccurate prediction for std(log10(MAH)) at lgmp = {0:.2f}"
+        assert np.allclose(x, y, atol=0.15), msg.format(lgmp)
+
+    for im, lgmp in enumerate(lgmp_targets):
+        x, y = np.log10(var_dmhdt_targets[im, :]), np.log10(var_dmhdt_preds[im, :])
+        msg = "Inaccurate prediction for std(dMh/dt) at lgmp = {0:.2f}"
+        assert np.allclose(x, y, atol=0.15), msg.format(lgmp)
