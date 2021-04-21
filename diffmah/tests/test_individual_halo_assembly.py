e401c5ce9eb22ca53811c3bca1d560f9f8d8be91
Andrew Hearin <ahearin@anl.gov>
2021-04-21 14:46:54 -0500

    Finished a few hard-coded unit tests

--- diffmah/tests/test_individual_halo_assembly.py
+++ diffmah/tests/test_individual_halo_assembly.py
@@ -4,7 +4,6 @@ import os
 import numpy as np
 from ..individual_halo_assembly import _calc_halo_history, DEFAULT_MAH_PARAMS
 from ..individual_halo_assembly import _power_law_index_vs_logt
-from ..rockstar_pdf_model import _get_cov_params_early, _get_cov_params_late
 from ..rockstar_pdf_model import _get_mean_mah_params_early, _get_mean_mah_params_late
 
 
@@ -26,13 +25,34 @@ def test_rolling_index_agrees_with_hard_coded_expectation():
     lgmp_test = 12.5
 
     k = DEFAULT_MAH_PARAMS["mah_k"]
-    logt_bn = "logt_testing_array_logmp_{0:.1f}.dat".format(lgmp_test)
+    logt_bn = "logt_testing_array.dat"
     logt = np.loadtxt(os.path.join(DDRN, logt_bn))
     logt0 = logt[-1]
 
     lge_e, lgl_e, lgtc_e = _get_mean_mah_params_early(lgmp_test)
     lge_l, lgl_l, lgtc_l = _get_mean_mah_params_late(lgmp_test)
-    _z1 = np.zeros(1)
-    cov_p_e = _get_cov_params_early(_z1 + lgmp_test)
-    cov_p_l = _get_cov_params_late(_z1 + lgmp_test)
-    # rolling_index = _rolling_plaw_vs_logt(logt, logt0, logmp, logtc, k, early, late)
+
+    indx_e_bn = "rolling_plaw_index_vs_time_rockstar_default_logmp_{0:.1f}_early.dat"
+    index_early_correct = np.loadtxt(os.path.join(DDRN, indx_e_bn.format(lgmp_test)))
+    index_early = _power_law_index_vs_logt(logt, lgtc_e, k, 10 ** lge_e, 10 ** lgl_e)
+    assert np.allclose(index_early_correct, index_early, rtol=0.01)
+
+    indx_l_bn = "rolling_plaw_index_vs_time_rockstar_default_logmp_{0:.1f}_late.dat"
+    index_late_correct = np.loadtxt(os.path.join(DDRN, indx_l_bn.format(lgmp_test)))
+    index_late = _power_law_index_vs_logt(logt, lgtc_l, k, 10 ** lge_l, 10 ** lgl_l)
+    assert np.allclose(index_late_correct, index_late, rtol=0.01)
+
+    dmhdt_e, log_mah_e = _calc_halo_history(
+        logt, logt0, lgmp_test, lgtc_e, k, 10 ** lge_e, 10 ** lgl_e
+    )
+    dmhdt_l, log_mah_l = _calc_halo_history(
+        logt, logt0, lgmp_test, lgtc_l, k, 10 ** lge_l, 10 ** lgl_l
+    )
+
+    log_mah_e_bn = "log_mah_vs_time_rockstar_default_logmp_{0:.1f}_early.dat"
+    log_mah_e_correct = np.loadtxt(os.path.join(DDRN, log_mah_e_bn.format(lgmp_test)))
+    assert np.allclose(log_mah_e_correct, log_mah_e, rtol=0.01)
+
+    log_mah_l_bn = "log_mah_vs_time_rockstar_default_logmp_{0:.1f}_late.dat"
+    log_mah_l_correct = np.loadtxt(os.path.join(DDRN, log_mah_l_bn.format(lgmp_test)))
+    assert np.allclose(log_mah_l_correct, log_mah_l, rtol=0.01)
