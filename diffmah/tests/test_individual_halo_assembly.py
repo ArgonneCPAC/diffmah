b845f7aae0147c4773103e6f74ac1bca57e4047d
Andrew Hearin <ahearin@anl.gov>
2021-04-21 13:51:27 -0500

    Folding in more hard-coded unit tests

--- diffmah/tests/test_individual_halo_assembly.py
+++ diffmah/tests/test_individual_halo_assembly.py
@@ -4,7 +4,7 @@ import numpy as np
 from ..individual_halo_assembly import _calc_halo_history, DEFAULT_MAH_PARAMS
 
 
-def test_something_evaluates():
+def test_calc_halo_history_evaluates():
     tarr = np.linspace(0.1, 14, 500)
     logt = np.log10(tarr)
     logtmp = logt[-1]
@@ -12,3 +12,7 @@ def test_something_evaluates():
     dmhdt, log_mah = _calc_halo_history(
         logt, logtmp, logmp, *DEFAULT_MAH_PARAMS.values()
     )
+
+
+def test_rolling_index_agrees_with_hard_coded_expectation():
+    pass
