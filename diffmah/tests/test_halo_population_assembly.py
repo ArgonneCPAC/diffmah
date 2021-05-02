bcdaf925060f53516abffea6479861d5136356a4
Andrew Hearin <ahearin@anl.gov>
2021-05-02 08:54:44 -0500

    Added SV testing for TNG

--- diffmah/tests/test_halo_population_assembly.py
+++ diffmah/tests/test_halo_population_assembly.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 from ..halo_population_assembly import _get_bimodal_halo_history
 from ..halo_population_assembly import UE_ARR, UL_ARR, LGTC_ARR
+from ..tng_pdf_model import DEFAULT_MAH_PDF_PARAMS as TNG_PARAMS
 
 _THIS_DRNAME = os.path.dirname(os.path.abspath(__file__))
 DDRN = os.path.join(_THIS_DRNAME, "testing_data")
@@ -70,20 +71,80 @@ def test_average_halo_histories_agree_with_nbody_simulations():
 
     for im, lgmp in enumerate(lgmp_targets):
         x, y = mean_log_mah_targets[im, :], mean_log_mah_preds[im, :]
-        msg = "Inaccurate prediction for <log10(MAH)> at lgmp = {0:.2f}"
+        msg = "Inaccurate N-body prediction for <log10(MAH)> at lgmp = {0:.2f}"
         assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
 
     for im, lgmp in enumerate(lgmp_targets):
         x, y = np.log10(mean_dmhdt_targets[im, :]), np.log10(mean_dmhdt_preds[im, :])
-        msg = "Inaccurate prediction for <dMh/dt> at lgmp = {0:.2f}"
+        msg = "Inaccurate N-body prediction for <dMh/dt> at lgmp = {0:.2f}"
         assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
 
     for im, lgmp in enumerate(lgmp_targets):
         x, y = var_log_mah_targets[im, :], var_log_mah_preds[im, :]
-        msg = "Inaccurate prediction for std(log10(MAH)) at lgmp = {0:.2f}"
+        msg = "Inaccurate N-body prediction for std(log10(MAH)) at lgmp = {0:.2f}"
         assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
 
     for im, lgmp in enumerate(lgmp_targets):
         x, y = np.log10(var_dmhdt_targets[im, :]), np.log10(var_dmhdt_preds[im, :])
-        msg = "Inaccurate prediction for std(dMh/dt) at lgmp = {0:.2f}"
+        msg = "Inaccurate N-body prediction for std(dMh/dt) at lgmp = {0:.2f}"
         assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
+
+
+def test_average_halo_histories_agree_with_tng():
+    mlist = list(
+        (
+            "11.50",
+            "11.75",
+            "12.00",
+            "12.25",
+            "12.50",
+            "12.75",
+            "13.00",
+            "13.25",
+            "13.50",
+            "13.75",
+        )
+    )
+    lgmp_targets = np.array([float(lgm) for lgm in mlist])
+    lgt = np.log10(np.loadtxt(os.path.join(DDRN, "tng_t_target.dat")))
+    mah_pat = "mean_log_mah_tng_logmp_{}.dat"
+    lgmah_fnames = list((os.path.join(DDRN, mah_pat.format(lgm)) for lgm in mlist))
+    mean_log_mah_targets = np.array([np.loadtxt(fn) for fn in lgmah_fnames])
+
+    vmah_pat = "var_log_mah_tng_logmp_{}.dat"
+    vlgmah_fnames = list((os.path.join(DDRN, vmah_pat.format(lgm)) for lgm in mlist))
+    var_log_mah_targets = np.array([np.loadtxt(fn) for fn in vlgmah_fnames])
+
+    dmhdt_pat = "mean_dmhdt_tng_logmp_{}.dat"
+    dmhdt_fnames = list((os.path.join(DDRN, dmhdt_pat.format(lgm)) for lgm in mlist))
+    mean_dmhdt_targets = np.array([np.loadtxt(fn) for fn in dmhdt_fnames])
+
+    vdmhdt_pat = "var_dmhdt_tng_logmp_{}.dat"
+    vdmhdt_fnames = list((os.path.join(DDRN, vdmhdt_pat.format(lgm)) for lgm in mlist))
+    var_dmhdt_targets = np.array([np.loadtxt(fn) for fn in vdmhdt_fnames])
+
+    _res = _get_bimodal_halo_history(
+        lgt, lgmp_targets, UE_ARR, UL_ARR, LGTC_ARR, **TNG_PARAMS
+    )
+    mean_dmhdt_preds, mean_log_mah_preds = _res[0], _res[2]
+    var_dmhdt_preds, var_log_mah_preds = _res[3], _res[4]
+
+    for im, lgmp in enumerate(lgmp_targets):
+        x, y = mean_log_mah_targets[im, :], mean_log_mah_preds[im, :]
+        msg = "Inaccurate TNG prediction for <log10(MAH)> at lgmp = {0:.2f}"
+        assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
+
+    for im, lgmp in enumerate(lgmp_targets):
+        x, y = np.log10(mean_dmhdt_targets[im, :]), np.log10(mean_dmhdt_preds[im, :])
+        msg = "Inaccurate TNG prediction for <dMh/dt> at lgmp = {0:.2f}"
+        assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
+
+    for im, lgmp in enumerate(lgmp_targets):
+        x, y = var_log_mah_targets[im, :], var_log_mah_preds[im, :]
+        msg = "Inaccurate TNG prediction for std(log10(MAH)) at lgmp = {0:.2f}"
+        assert np.allclose(x, y, atol=0.1), msg.format(lgmp)
+
+    for im, lgmp in enumerate(lgmp_targets):
+        x, y = np.log10(var_dmhdt_targets[im, :]), np.log10(var_dmhdt_preds[im, :])
+        msg = "Inaccurate TNG prediction for std(dMh/dt) at lgmp = {0:.2f}"
+        assert np.allclose(x, y, atol=0.2), msg.format(lgmp)
