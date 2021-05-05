927866f75983a46fed0c8283cad56ddfb3993bf1
Andrew Hearin <ahearin@anl.gov>
2021-05-05 07:54:29 -0500

    Changed API of jax_adam_wrapper to accept loss_and_grad function. Should resolve #83.

--- diffmah/tests/test_utils.py
+++ diffmah/tests/test_utils.py
@@ -3,12 +3,12 @@
 import numpy as np
 from jax import numpy as jax_np
 from jax import jit as jax_jit
+from jax import value_and_grad
 from ..utils import jax_inverse_sigmoid, jax_sigmoid, jax_adam_wrapper
 
 
 def test_inverse_sigmoid_actually_inverts():
-    """
-    """
+    """"""
     x0, k, ylo, yhi = 0, 5, 1, 0
     xarr = np.linspace(-1, 1, 100)
     yarr = np.array(jax_sigmoid(xarr, x0, k, ylo, yhi))
@@ -27,13 +27,17 @@ def test_jax_adam_wrapper_actually_minimizes_the_loss():
         loss = jax_np.sum(diff * diff) / diff.size
         return loss
 
+    @jax_jit
+    def mse_loss_and_grad(params, data):
+        return value_and_grad(mse_loss, argnums=0)(params, data)
+
     params_init = np.array((2.75, 0.75))
     x = np.linspace(-1, 1, 50)
     data = (x,)
     loss_init = mse_loss(params_init, data)
     n_step = 100
     params_bestfit, loss_bestfit, loss_arr, params_arr, flag = jax_adam_wrapper(
-        mse_loss, params_init, data, n_step, step_size=0.01
+        mse_loss_and_grad, params_init, data, n_step, step_size=0.01
     )
     assert loss_arr[-1] < loss_init
     assert np.allclose(loss_bestfit, loss_arr[-1], atol=0.001)
