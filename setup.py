e1bbf48f84b215a4e71cd5f389aac46fc002b2d4
Andrew Hearin <ahearin@anl.gov>
2022-08-29 10:21:04 -0500

    Add _version module so that diffmah has __version__ attribute

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
+import os
 from setuptools import setup, find_packages
 
 
-PACKAGENAME = "diffmah"
-VERSION = "0.3.0"
+__version__ = None
+pth = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "diffmah", "_version.py"
+)
+with open(pth, "r") as fp:
+    exec(fp.read())
 
 
 setup(
-    name=PACKAGENAME,
-    version=VERSION,
+    name="diffmah",
+    version=__version__,
     author="Andrew Hearin",
     author_email="ahearin@anl.gov",
     description="Differentiable model of dark matter halo assembly",
-    long_description="Differentiable model of dark matter halo assembly",
     install_requires=["numpy", "jax"],
     packages=find_packages(),
     url="https://github.com/ArgonneCPAC/diffmah",
