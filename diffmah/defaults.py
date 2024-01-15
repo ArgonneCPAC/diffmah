ba661373181d7634fabf08ac21c111fe5dc8d25b
Andrew Hearin <ahearin@anl.gov>
2024-01-15 14:52:34 -0600

    Eliminate inconsistent usage of DEFAULT_MAH_PARAMS throughout rest of package

--- diffmah/defaults.py
+++ diffmah/defaults.py
@@ -12,3 +12,5 @@ LGT0 = np.log10(TODAY)
 DEFAULT_MAH_PDICT = OrderedDict(logmp=12.0, logtc=0.05, early_index=2.5, late_index=1.0)
 DiffmahParams = namedtuple("DiffmahParams", list(DEFAULT_MAH_PDICT.keys()))
 DEFAULT_MAH_PARAMS = DiffmahParams(*list(DEFAULT_MAH_PDICT.values()))
+
+MAH_K = 3.5
