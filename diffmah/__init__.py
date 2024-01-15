b4cfe0d429142507de44dfd97211aba360ac1fc5
Andrew Hearin <ahearin@anl.gov>
2024-01-15 16:17:28 -0600

    Add top-level imports

--- diffmah/__init__.py
+++ diffmah/__init__.py
@@ -3,6 +3,7 @@
 # flake8: noqa
 
 from ._version import __version__
-from .individual_halo_assembly import calc_halo_history
+from .defaults import DEFAULT_MAH_PARAMS, MAH_K, DiffmahParams
+from .individual_halo_assembly import calc_halo_history, mah_halopop, mah_singlehalo
 from .monte_carlo_diffmah_hiz import mc_diffmah_params_hiz
 from .monte_carlo_halo_population import mc_halo_population
---
+++ diffmah/tests/test_defaults.py
@@ -0,0 +1,26 @@
+"""
+"""
+import numpy as np
+
+
+def test_default_mah_params_imports_from_top_level_and_is_frozen():
+    from .. import DEFAULT_MAH_PARAMS
+
+    assert np.allclose(DEFAULT_MAH_PARAMS, (12.0, 0.05, 2.6137643, 0.12692805))
+
+
+def test_mah_k_imports_from_top_level():
+    from .. import MAH_K
+
+    assert np.allclose(MAH_K, 3.5)
+
+
+def test_mah_halopop_imports_from_top_level():
+    from .. import DEFAULT_MAH_PARAMS, DiffmahParams, mah_halopop
+
+    tarr = np.linspace(0.1, 13.7, 100)
+    ngals = 150
+    zz = np.zeros(ngals)
+    mah_params_halopop = DiffmahParams(*[x + zz for x in DEFAULT_MAH_PARAMS])
+    dmhdt, log_mah = mah_halopop(mah_params_halopop, tarr)
+    assert log_mah.shape == dmhdt.shape
