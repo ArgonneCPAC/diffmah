65cde3732ca107fdb3e4751356600d57967f69f6
Andrew Hearin <ahearin@anl.gov>
2024-01-15 15:55:24 -0600

    Change values of DEFAULT_MAH_PARAMS to be unchanged from previous version

--- diffmah/defaults.py
+++ diffmah/defaults.py
@@ -9,7 +9,9 @@ TODAY = 13.8
 LGT0 = np.log10(TODAY)
 
 
-DEFAULT_MAH_PDICT = OrderedDict(logmp=12.0, logtc=0.05, early_index=2.5, late_index=1.0)
+DEFAULT_MAH_PDICT = OrderedDict(
+    logmp=12.0, logtc=0.05, early_index=2.6137643, late_index=0.12692805
+)
 DiffmahParams = namedtuple("DiffmahParams", list(DEFAULT_MAH_PDICT.keys()))
 DEFAULT_MAH_PARAMS = DiffmahParams(*list(DEFAULT_MAH_PDICT.values()))
 
