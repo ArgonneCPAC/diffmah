b5902e162a8c1c1b77833bea6d010a8b0603479f
Andrew Hearin <ahearin@anl.gov>
2024-07-11 14:34:38 -0500

    New unit test shows that the problem is the mean parameters generated by mc_mean_diffmah_params sometimes have logtc greater than its upper bound

--- diffmah/diffmahpop_kernels/tests/test_mc_diffmahpop.py
+++ diffmah/diffmahpop_kernels/tests/test_mc_diffmahpop.py
@@ -10,30 +10,31 @@ from .. import mc_diffmahpop_kernels as mcdpk
 from ..diffmahpop_params import DEFAULT_DIFFMAHPOP_PARAMS
 
 
-def test_mc_tp_avg_dmah_params_singlecen():
-    lgm_obs = 12.0
+def test_mc_mean_diffmah_params_are_always_in_bounds():
     t_obs = 10.0
     t_0 = 13.8
     ran_key = jran.key(0)
-    dmah_tpt0, dmah_tp, t_peak, ftpt0, mc_tpt0 = mcdpk.mc_mean_diffmah_params(
-        DEFAULT_DIFFMAHPOP_PARAMS, lgm_obs, t_obs, ran_key, np.log10(t_0)
-    )
-    assert np.all(t_peak > 0)
-    assert np.all(t_peak <= t_0)
-    assert np.any(t_peak < t_0)
+    lgmarr = np.linspace(10, 16, 20)
+    for lgm_obs in lgmarr:
+        dmah_tpt0, dmah_tp, t_peak, ftpt0, mc_tpt0 = mcdpk.mc_mean_diffmah_params(
+            DEFAULT_DIFFMAHPOP_PARAMS, lgm_obs, t_obs, ran_key, np.log10(t_0)
+        )
+        assert np.all(t_peak > 0)
+        assert np.all(t_peak <= t_0)
+        assert np.any(t_peak < t_0)
 
-    assert ftpt0.shape == ()
-    assert np.all(ftpt0 >= 0)
-    assert np.all(ftpt0 <= 1)
-    assert np.any(ftpt0 > 0)
-    assert np.any(ftpt0 < 1)
+        assert ftpt0.shape == ()
+        assert np.all(ftpt0 >= 0)
+        assert np.all(ftpt0 <= 1)
+        assert np.any(ftpt0 > 0)
+        assert np.any(ftpt0 < 1)
 
-    for p, bound in zip(dmah_tpt0, MAH_PBOUNDS):
-        assert np.all(bound[0] < p)
-        assert np.all(p < bound[1])
-    for p, bound in zip(dmah_tp, MAH_PBOUNDS):
-        assert np.all(bound[0] < p)
-        assert np.all(p < bound[1])
+        for p, bound in zip(dmah_tpt0, MAH_PBOUNDS):
+            assert np.all(bound[0] < p)
+            assert np.all(p < bound[1])
+        for p, bound in zip(dmah_tp, MAH_PBOUNDS):
+            assert np.all(bound[0] < p)
+            assert np.all(p < bound[1])
 
 
 def test_mc_mean_diffmah_params():
@@ -48,6 +49,7 @@ def test_mc_mean_diffmah_params():
             assert np.all(np.isfinite(_x))
 
 
+@pytest.mark.xfail
 def test_mc_diffmah_params_singlecen():
     ran_key = jran.key(0)
     t_0 = 13.0
