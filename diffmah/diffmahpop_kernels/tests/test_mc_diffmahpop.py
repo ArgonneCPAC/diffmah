dc5981e85127495e892cb89792b0de39bae05873
Andrew Hearin <ahearin@anl.gov>
2024-07-11 14:30:21 -0500

    Add failing test of mc_diffmah_params_singlecen

--- diffmah/diffmahpop_kernels/tests/test_mc_diffmahpop.py
+++ diffmah/diffmahpop_kernels/tests/test_mc_diffmahpop.py
@@ -36,16 +36,30 @@ def test_mc_tp_avg_dmah_params_singlecen():
         assert np.all(p < bound[1])
 
 
-def test_mc_diffmah_params_singlecen():
-    lgm_obs = 12.0
+def test_mc_mean_diffmah_params():
     t_obs = 10.0
     t_0 = 13.8
     ran_key = jran.key(0)
-    args = DEFAULT_DIFFMAHPOP_PARAMS, lgm_obs, t_obs, ran_key, np.log10(t_0)
-    _res = mcdpk.mc_mean_diffmah_params(*args)
-    ran_diffmah_params_tpt0, ran_diffmah_params_tp, t_peak, ftpt0, mc_tpt0 = _res
-    for _x in _res:
-        assert np.all(np.isfinite(_x))
+    for lgm_obs in np.linspace(10, 16, 20):
+        args = DEFAULT_DIFFMAHPOP_PARAMS, lgm_obs, t_obs, ran_key, np.log10(t_0)
+        _res = mcdpk.mc_mean_diffmah_params(*args)
+        ran_diffmah_params_tpt0, ran_diffmah_params_tp, t_peak, ftpt0, mc_tpt0 = _res
+        for _x in _res:
+            assert np.all(np.isfinite(_x))
+
+
+def test_mc_diffmah_params_singlecen():
+    ran_key = jran.key(0)
+    t_0 = 13.0
+    lgt0 = np.log10(t_0)
+    t_obs = 10.0
+    lgmarr = np.linspace(10, 15, 20)
+    for lgm_obs in lgmarr:
+        args = (DEFAULT_DIFFMAHPOP_PARAMS, lgm_obs, t_obs, ran_key, lgt0)
+        _res = mcdpk.mc_diffmah_params_singlecen(*args)
+        mah_params_tpt0, mah_params_tp, t_peak, ftpt0, mc_tpt0 = _res
+        assert np.all(np.isfinite(mah_params_tpt0.logtc))
+        assert np.all(np.isfinite(mah_params_tp.logtc))
 
 
 @pytest.mark.xfail
@@ -63,6 +77,7 @@ def test_predict_mah_moments_singlebin():
         assert np.all(np.isfinite(std_log_mah))
 
 
+@pytest.mark.xfail
 def test_mc_diffmah_halo_sample():
     ran_key = jran.key(0)
     t_0 = 13.0
