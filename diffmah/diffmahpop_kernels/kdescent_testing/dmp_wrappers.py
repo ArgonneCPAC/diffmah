4cc5d72590943ff8de7018e921ee2c78a716f8d0
Andrew Hearin <ahearin@anl.gov>
2024-07-15 11:58:09 -0500

    Begin to build unit test of kdescent application

---
+++ diffmah/diffmahpop_kernels/kdescent_testing/dmp_wrappers.py
@@ -0,0 +1,20 @@
+"""
+"""
+
+from jax import jit as jjit
+
+from .. import diffmahpop_params as dpp
+from .. import mc_diffmahpop_kernels as mdk
+
+
+@jjit
+def mc_diffmah_preds(diffmahpop_u_params, pred_data):
+    diffmahpop_params = dpp.get_diffmahpop_params_from_u_params(diffmahpop_u_params)
+    tarr, lgm_obs, t_obs, ran_key, lgt0 = pred_data
+    _res = mdk._mc_diffmah_halo_sample(
+        diffmahpop_params, tarr, lgm_obs, t_obs, ran_key, lgt0
+    )
+    ftpt0 = _res[3]
+    log_mah_tpt0 = _res[6]
+    log_mah_tp = _res[8]
+    return log_mah_tpt0, log_mah_tp, ftpt0
---
+++ diffmah/diffmahpop_kernels/kdescent_testing/tests/test_kdescent_application.py
@@ -0,0 +1,42 @@
+"""
+"""
+
+import numpy as np
+import pytest
+from jax import numpy as jnp
+from jax import random as jran
+
+from ... import diffmahpop_params as dpp
+from ..dmp_wrappers import mc_diffmah_preds
+
+try:
+    import kdescent
+
+    HAS_KDESCENT = True
+except ImportError:
+    HAS_KDESCENT = False
+
+
+@pytest.mark.skipif("not HAS_KDESCENT")
+def test_fit_diffmah_to_itself_with_kdescent():
+    ran_key = jran.key(0)
+
+    # use default params as the initial guess
+    u_p_init = dpp.DEFAULT_DIFFMAHPOP_U_PARAMS._make(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS)
+
+    # Use randomly diffmahpop parameter to generate fiducial data
+    u_p_fid_key, ran_key = jran.split(ran_key, 2)
+    n_params = len(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS)
+    uran = jran.uniform(u_p_fid_key, minval=-1, maxval=1, shape=(n_params,))
+    _u_p_list = [x + u for x, u in zip(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS, uran)]
+    u_p_fid = dpp.DEFAULT_DIFFMAHPOP_U_PARAMS._make(jnp.array(_u_p_list))
+
+    t_0 = 13.8
+    lgt0 = np.log10(t_0)
+    lgm_obs = 12.0
+    t_obs = 10.0
+    num_target_redshifts_per_t_obs = 10
+    tarr = np.linspace(0.5, t_obs, num_target_redshifts_per_t_obs)
+    pred_data = tarr, lgm_obs, t_obs, ran_key, lgt0
+    _res = mc_diffmah_preds(u_p_init, pred_data)
+    log_mah_tpt0, log_mah_tp, ftpt0 = _res
