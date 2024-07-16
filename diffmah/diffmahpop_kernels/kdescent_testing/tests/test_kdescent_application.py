8b76b14940d5437999a69b7326383c58cde7f018
Andrew Hearin <ahearin@anl.gov>
2024-07-16 11:47:18 -0500

    add failing test of kdescent.adam

--- diffmah/diffmahpop_kernels/kdescent_testing/tests/test_kdescent_application.py
+++ diffmah/diffmahpop_kernels/kdescent_testing/tests/test_kdescent_application.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import pytest
+from jax import jit as jjit
 from jax import numpy as jnp
 from jax import random as jran
 
@@ -16,15 +17,19 @@ try:
 except ImportError:
     HAS_KDESCENT = False
 
+T_MIN_FIT = 0.5
+
 
 @pytest.mark.skipif("not HAS_KDESCENT")
 def test_fit_diffmah_to_itself_with_kdescent():
+    """Enforce that single-sample loss has finite grads"""
     ran_key = jran.key(0)
 
     # Use random diffmahpop parameter to generate fiducial data
+    DP = 1.0
     u_p_fid_key, ran_key = jran.split(ran_key, 2)
     n_params = len(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS)
-    uran = jran.uniform(u_p_fid_key, minval=-1.0, maxval=1.0, shape=(n_params,))
+    uran = jran.uniform(u_p_fid_key, minval=-DP, maxval=DP, shape=(n_params,))
     _u_p_list = [x + u for x, u in zip(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS, uran)]
     u_p_fid = dpp.DEFAULT_DIFFMAHPOP_U_PARAMS._make(jnp.array(_u_p_list))
 
@@ -33,7 +38,7 @@ def test_fit_diffmah_to_itself_with_kdescent():
     lgm_obs = 12.0
     t_obs = 10.0
     num_target_redshifts_per_t_obs = 10
-    tarr = np.linspace(0.5, t_obs, num_target_redshifts_per_t_obs)
+    tarr = np.linspace(T_MIN_FIT, t_obs, num_target_redshifts_per_t_obs)
 
     _res = dmpw.get_single_sample_self_fit_target_data(
         u_p_fid, tarr, lgm_obs, t_obs, ran_key, lgt0
@@ -54,12 +59,14 @@ def test_fit_diffmah_to_itself_with_kdescent():
 
 @pytest.mark.skipif("not HAS_KDESCENT")
 def test_multisample_kde_loss_self_fit():
+    """Enforce that multi-sample loss has finite grads"""
     ran_key = jran.key(0)
 
     # Use random diffmahpop parameter to generate fiducial data
+    DP = 0.1
     u_p_fid_key, ran_key = jran.split(ran_key, 2)
     n_params = len(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS)
-    uran = jran.uniform(u_p_fid_key, minval=-0.2, maxval=0.2, shape=(n_params,))
+    uran = jran.uniform(u_p_fid_key, minval=-DP, maxval=DP, shape=(n_params,))
     _u_p_list = [x + u for x, u in zip(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS, uran)]
     u_p_fid = dpp.DEFAULT_DIFFMAHPOP_U_PARAMS._make(jnp.array(_u_p_list))
 
@@ -72,7 +79,7 @@ def test_multisample_kde_loss_self_fit():
     num_target_redshifts_per_t_obs = 10
 
     tarr_matrix = jnp.array(
-        [jnp.linspace(0.5, t, num_target_redshifts_per_t_obs) for t in tobsarr]
+        [jnp.linspace(T_MIN_FIT, t, num_target_redshifts_per_t_obs) for t in tobsarr]
     )
     _keys = jran.split(ran_key, num_samples * 2)
     _res = dmpw.get_multisample_self_fit_target_data(
@@ -96,3 +103,60 @@ def test_multisample_kde_loss_self_fit():
 
     loss, grads = dmpw.multisample_kde_loss_and_grad_self_fit(u_p_init, *loss_data)
     assert np.all(np.isfinite(grads))
+
+
+@pytest.mark.skipif("not HAS_KDESCENT")
+def test_kdescent_adam_self_fit():
+    """Enforce that kdescent.adam terminates without NaNs"""
+    ran_key = jran.key(0)
+
+    # Use random diffmahpop parameter to generate fiducial data
+    DP = 0.01
+    u_p_fid_key, ran_key = jran.split(ran_key, 2)
+    n_params = len(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS)
+    uran = jran.uniform(u_p_fid_key, minval=-DP, maxval=DP, shape=(n_params,))
+    _u_p_list = [x + u for x, u in zip(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS, uran)]
+    u_p_fid = dpp.DEFAULT_DIFFMAHPOP_U_PARAMS._make(jnp.array(_u_p_list))
+
+    lgmobs_key, tobs_key, ran_key = jran.split(ran_key, 3)
+    num_samples = 5
+    t_0 = 13.8
+    lgt0 = np.log10(t_0)
+    lgmobsarr = jran.uniform(lgmobs_key, minval=11, maxval=15, shape=(num_samples,))
+    tobsarr = jran.uniform(tobs_key, minval=4, maxval=13, shape=(num_samples,))
+    num_target_redshifts_per_t_obs = 10
+
+    tarr_matrix = jnp.array(
+        [jnp.linspace(T_MIN_FIT, t, num_target_redshifts_per_t_obs) for t in tobsarr]
+    )
+
+    @jjit
+    def kde_loss(u_p, randkey):
+        _keys = jran.split(randkey, num_samples * 2)
+        _res = dmpw.get_multisample_self_fit_target_data(
+            u_p_fid, tarr_matrix, lgmobsarr, tobsarr, _keys[:num_samples], lgt0
+        )
+        log_mahs_targets, weights_targets = _res
+        loss_data = (
+            tarr_matrix,
+            lgmobsarr,
+            tobsarr,
+            _keys[num_samples:],
+            lgt0,
+            log_mahs_targets,
+            weights_targets,
+        )
+        return dmpw.multisample_kde_loss_self_fit(u_p, *loss_data)
+
+    # use default params as the initial guess
+    u_p_init = dpp.DEFAULT_DIFFMAHPOP_U_PARAMS._make(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS)
+
+    adam_results = kdescent.adam(
+        kde_loss,
+        u_p_init,
+        nsteps=5,
+        learning_rate=0.1,
+        randkey=12345,
+    )
+    u_p_best = adam_results[-1]
+    assert np.all(np.isfinite(u_p_best))
