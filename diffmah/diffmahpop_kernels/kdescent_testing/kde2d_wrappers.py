ce7edadfa400bc87626e5dc6dd3eebc954385c73
Andrew Hearin <ahearin@anl.gov>
2024-07-18 14:36:36 -0500

    Implement 2d single_sample_kde_loss_self_fit. Next steps are to fill out the remaining kernels, and then switch to logarithmic sMAR

--- diffmah/diffmahpop_kernels/kdescent_testing/kde2d_wrappers.py
+++ diffmah/diffmahpop_kernels/kdescent_testing/kde2d_wrappers.py
@@ -30,3 +30,83 @@ def mc_diffmah_preds(diffmahpop_u_params, pred_data):
     dmhdt_tp = _res[7]
     log_mah_tp = _res[8]
     return dmhdt_tpt0, log_mah_tpt0, dmhdt_tp, log_mah_tp, ftpt0
+
+
+@jjit
+def get_single_sample_self_fit_target_data(
+    u_params, tarr, lgm_obs, t_obs, ran_key, lgt0
+):
+    pred_data = tarr, lgm_obs, t_obs, ran_key, lgt0
+    _res = mc_diffmah_preds(u_params, pred_data)
+    dmhdt_tpt0, log_mah_tpt0, dmhdt_tp, log_mah_tp, ftpt0 = _res
+    weights_target = jnp.concatenate((ftpt0, 1 - ftpt0))
+    dmhdts_target = jnp.concatenate((dmhdt_tpt0, dmhdt_tp))
+    log_mahs_target = jnp.concatenate((log_mah_tpt0, log_mah_tp))
+    X_target = jnp.array((dmhdts_target, log_mahs_target)).swapaxes(0, 1)
+    return X_target, weights_target
+
+
+@jjit
+def single_sample_kde_loss_self_fit(
+    diffmahpop_u_params,
+    tarr,
+    lgm_obs,
+    t_obs,
+    ran_key,
+    lgt0,
+    X_target,
+    weights_target,
+):
+    kcalc0 = kdescent.KCalc(X_target[:, :, 0], weights_target)
+    kcalc1 = kdescent.KCalc(X_target[:, :, 1], weights_target)
+    kcalc2 = kdescent.KCalc(X_target[:, :, 2], weights_target)
+    kcalc3 = kdescent.KCalc(X_target[:, :, 3], weights_target)
+    kcalc4 = kdescent.KCalc(X_target[:, :, 4], weights_target)
+
+    ran_key, pred_key = jran.split(ran_key, 2)
+    pred_data = tarr, lgm_obs, t_obs, pred_key, lgt0
+    _res = mc_diffmah_preds(diffmahpop_u_params, pred_data)
+    dmhdt_tpt0, log_mah_tpt0, dmhdt_tp, log_mah_tp, ftpt0 = _res
+
+    weights_pred = jnp.concatenate((ftpt0, 1 - ftpt0))
+    dmhdts_pred = jnp.concatenate((dmhdt_tpt0, dmhdt_tp))
+    log_mahs_pred = jnp.concatenate((log_mah_tpt0, log_mah_tp))
+    X_preds = jnp.array((dmhdts_pred, log_mahs_pred)).swapaxes(0, 1)
+
+    kcalc_keys = jran.split(ran_key, N_T_PER_BIN)
+
+    model_counts0, truth_counts0 = kcalc0.compare_kde_counts(
+        kcalc_keys[0], X_preds[:, :, 0], weights_pred
+    )
+    model_counts1, truth_counts1 = kcalc1.compare_kde_counts(
+        kcalc_keys[1], X_preds[:, :, 1], weights_pred
+    )
+    model_counts2, truth_counts2 = kcalc2.compare_kde_counts(
+        kcalc_keys[2], X_preds[:, :, 2], weights_pred
+    )
+    model_counts3, truth_counts3 = kcalc3.compare_kde_counts(
+        kcalc_keys[3], X_preds[:, :, 3], weights_pred
+    )
+    model_counts4, truth_counts4 = kcalc4.compare_kde_counts(
+        kcalc_keys[4], X_preds[:, :, 4], weights_pred
+    )
+
+    diff0 = model_counts0 - truth_counts0
+    diff1 = model_counts1 - truth_counts1
+    diff2 = model_counts2 - truth_counts2
+    diff3 = model_counts3 - truth_counts3
+    diff4 = model_counts4 - truth_counts4
+
+    loss0 = jnp.mean(diff0**2)
+    loss1 = jnp.mean(diff1**2)
+    loss2 = jnp.mean(diff2**2)
+    loss3 = jnp.mean(diff3**2)
+    loss4 = jnp.mean(diff4**2)
+
+    loss = loss0 + loss1 + loss2 + loss3 + loss4
+    return loss
+
+
+single_sample_kde_loss_and_grad_self_fit = jjit(
+    value_and_grad(single_sample_kde_loss_self_fit)
+)
--- diffmah/diffmahpop_kernels/kdescent_testing/tests/test_kde2d_wrappers.py
+++ diffmah/diffmahpop_kernels/kdescent_testing/tests/test_kde2d_wrappers.py
@@ -43,3 +43,47 @@ def test_mc_diffmah_preds():
         assert np.all(log_mah_tp < 20)
         assert np.all(ftpt0 <= 1)
         assert np.all(ftpt0 >= 0)
+
+
+@pytest.mark.skipif("not HAS_KDESCENT")
+def test_single_sample_kde_loss_self_fit():
+    """Enforce that single-sample loss has finite grads"""
+    ran_key = jran.key(0)
+
+    t_0 = 13.8
+    lgt0 = np.log10(t_0)
+    DP = 0.5
+
+    n_tests = 100
+    for __ in range(n_tests):
+
+        # Use random diffmahpop parameter to generate fiducial data
+        u_p_fid_key, ran_key = jran.split(ran_key, 2)
+        n_params = len(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS)
+        uran = jran.uniform(u_p_fid_key, minval=-DP, maxval=DP, shape=(n_params,))
+        _u_p_list = [x + u for x, u in zip(dpp.DEFAULT_DIFFMAHPOP_U_PARAMS, uran)]
+        u_p_fid = dpp.DEFAULT_DIFFMAHPOP_U_PARAMS._make(jnp.array(_u_p_list))
+
+        ran_key, lgm_key, t_obs_key = jran.split(ran_key, 3)
+        lgm_obs = jran.uniform(lgm_key, minval=11, maxval=15, shape=())
+        t_obs = jran.uniform(t_obs_key, minval=4, maxval=t_0, shape=())
+
+        tarr = np.linspace(T_MIN_FIT, t_obs, k2w.N_T_PER_BIN)
+
+        _res = k2w.get_single_sample_self_fit_target_data(
+            u_p_fid, tarr, lgm_obs, t_obs, ran_key, lgt0
+        )
+        X_target, weights_target = _res
+
+        # use default params as the initial guess
+        u_p_init = dpp.DEFAULT_DIFFMAHPOP_U_PARAMS._make(
+            dpp.DEFAULT_DIFFMAHPOP_U_PARAMS
+        )
+
+        loss_data = tarr, lgm_obs, t_obs, ran_key, lgt0, X_target, weights_target
+        loss = k2w.single_sample_kde_loss_self_fit(u_p_init, *loss_data)
+        assert np.all(np.isfinite(loss)), (lgm_obs, t_obs)
+        assert loss > 0
+
+        loss, grads = k2w.single_sample_kde_loss_and_grad_self_fit(u_p_init, *loss_data)
+        assert np.all(np.isfinite(grads)), (lgm_obs, t_obs)
