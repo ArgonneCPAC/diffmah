10f015872b25be98bcdebe71789afab1c1556124
Andrew Hearin <ahearin@anl.gov>
2024-08-06 17:35:30 -0500

    Separate abscissa for cens and sats

--- diffmah/diffmahpop_kernels/monocensat_fithelp.py
+++ diffmah/diffmahpop_kernels/monocensat_fithelp.py
@@ -2,6 +2,7 @@
 """
 
 from jax import jit as jjit
+from jax import random as jran
 from jax import value_and_grad
 
 from . import monocens_fithelp, monosats_fithelp
@@ -10,9 +11,12 @@ from . import monocens_fithelp, monosats_fithelp
 @jjit
 def loss_mah_moments_multibin_censat(
     diffmahpop_params,
-    tarr_matrix,
-    lgm_obs_arr,
-    t_obs_arr,
+    tarr_matrix_cens,
+    lgm_obs_arr_cens,
+    t_obs_arr_cens,
+    tarr_matrix_sats,
+    lgm_obs_arr_sats,
+    t_obs_arr_sats,
     ran_key,
     lgt0,
     target_mean_log_mahs_cens,
@@ -22,12 +26,13 @@ def loss_mah_moments_multibin_censat(
     target_std_log_mahs_sats,
     target_frac_peaked_sats,
 ):
+    ran_key_cens, ran_key_sats = jran.split(ran_key, 2)
     loss_cens = monocens_fithelp.loss_mah_moments_multibin(
         diffmahpop_params,
-        tarr_matrix,
-        lgm_obs_arr,
-        t_obs_arr,
-        ran_key,
+        tarr_matrix_cens,
+        lgm_obs_arr_cens,
+        t_obs_arr_cens,
+        ran_key_cens,
         lgt0,
         target_mean_log_mahs_cens,
         target_std_log_mahs_cens,
@@ -36,10 +41,10 @@ def loss_mah_moments_multibin_censat(
 
     loss_sats = monosats_fithelp.loss_mah_moments_multibin(
         diffmahpop_params,
-        tarr_matrix,
-        lgm_obs_arr,
-        t_obs_arr,
-        ran_key,
+        tarr_matrix_sats,
+        lgm_obs_arr_sats,
+        t_obs_arr_sats,
+        ran_key_sats,
         lgt0,
         target_mean_log_mahs_sats,
         target_std_log_mahs_sats,
