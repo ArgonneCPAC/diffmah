0f8bd5b030a471271feeaecd5cebf11a5f39bfd5
Andrew Hearin <ahearin@anl.gov>
2024-12-14 13:05:54 -0600

    Remove calc_halo_history from top-level import

--- diffmah/__init__.py
+++ diffmah/__init__.py
@@ -6,4 +6,4 @@
 from ._version import __version__
 from .defaults import DEFAULT_MAH_PARAMS, MAH_K, DiffmahParams
 from .fitting_helpers import diffmah_fitter
-from .individual_halo_assembly import calc_halo_history, mah_halopop, mah_singlehalo
+from .individual_halo_assembly import mah_halopop, mah_singlehalo
