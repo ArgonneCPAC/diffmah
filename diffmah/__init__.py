b816f4d6084da92b2b9d07061c2d0ec28d12780e
Andrew Hearin <ahearin@anl.gov>
2024-12-14 13:05:03 -0600

    remove mc_halo_population from top-level import

--- diffmah/__init__.py
+++ diffmah/__init__.py
@@ -7,4 +7,3 @@ from ._version import __version__
 from .defaults import DEFAULT_MAH_PARAMS, MAH_K, DiffmahParams
 from .fitting_helpers import diffmah_fitter
 from .individual_halo_assembly import calc_halo_history, mah_halopop, mah_singlehalo
-from .monte_carlo_halo_population import mc_halo_population
