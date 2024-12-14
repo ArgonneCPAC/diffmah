47b286fbf1471f7a7db1085224d7ea3e2a7f6243
Andrew Hearin <ahearin@anl.gov>
2024-12-14 13:20:11 -0600

    Remove obsolete implementations of diffmah

--- diffmah/__init__.py
+++ diffmah/__init__.py
@@ -5,5 +5,5 @@
 
 from ._version import __version__
 from .defaults import DEFAULT_MAH_PARAMS, MAH_K, DiffmahParams
+from .diffmah_kernels import mah_halopop, mah_singlehalo
 from .fitting_helpers import diffmah_fitter
-from .individual_halo_assembly import mah_halopop, mah_singlehalo
