eb8d03a109cb6f0f67d4ea5cedb34141ed97de87
Andrew Hearin <ahearin@anl.gov>
2024-12-14 07:09:26 -0600

    Update Discovery fitter script to use new data_loader

--- scripts/hacc_discovery_sims_diffmah_fitter_script.py
+++ scripts/hacc_discovery_sims_diffmah_fitter_script.py
@@ -7,19 +7,20 @@ from glob import glob
 from time import time
 
 import numpy as np
-from diffmah import fitting_helpers as cfh
 from mpi4py import MPI
 
-from load_w0wa_cores import NCHUNKS, NUM_SUBVOLS_DISCOVERY, load_mahs_per_rank
+from diffmah import fitting_helpers as cfh
+from diffmah.data_loaders.load_hacc_mahs import load_mahs_per_rank
 
 TMP_OUTPAT = "tmp_mah_fits_rank_{0}.dat"
 
-LCRC = None
-TASSO_ROOT_DRN = "/Users/aphearin/work/DATA/DESI_W0WA"
-TASSO_LCDM_DRN = os.path.join(TASSO_ROOT_DRN, "LCDM")
-TASSO_W0WA_DRN = os.path.join(TASSO_ROOT_DRN, "W0WA")
+LCRC_DRN_PAT = "/lcrc/group/cosmodata/simulations/DESI_W0WA/{0}/coreforest/forest"
+POBOY_DRN_PAT = "/Users/aphearin/work/DATA/DESI_W0WA/{0}"
 
 BNPAT = "m000p.coreforest.{}.hdf5"
+
+NCHUNKS = 20
+NUM_SUBVOLS_DISCOVERY = 96
 
 
 if __name__ == "__main__":
@@ -27,12 +28,17 @@ if __name__ == "__main__":
     rank, nranks = comm.Get_rank(), comm.Get_size()
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("indir", help="Input directory")
+    parser.add_argument("indir", help="Root directory storing both Discovery sims")
+    parser.add_argument("sim_name", help="Simulation name", choices=["LCDM", "W0WA"])
     parser.add_argument("outdir", help="Output directory")
-    parser.add_argument("outbase", help="Basename of the output hdf5 file")
+    parser.add_argument(
+        "-outbase", help="Basename of the output hdf5 file", default="diffmah_fits.h5"
+    )
     parser.add_argument("-test", help="Short test run?", type=bool, default=False)
     parser.add_argument("-istart", help="First subvolume in loop", type=int, default=0)
-    parser.add_argument("-iend", help="Last subvolume in loop", type=int, default=-1)
+    parser.add_argument(
+        "-iend", help="Last subvolume in loop", type=int, default=NUM_SUBVOLS_DISCOVERY
+    )
     parser.add_argument("-nstep", help="Number of gd steps", type=int, default=200)
     parser.add_argument("-nchunks", help="Number of chunks", type=int, default=NCHUNKS)
     parser.add_argument(
@@ -43,6 +49,7 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    sim_name = args.sim_name
     nstep = args.nstep
     istart, iend = args.istart, args.iend
 
@@ -55,13 +62,13 @@ if __name__ == "__main__":
 
     os.makedirs(outdir, exist_ok=True)
 
-    if args.indir == "TASSO":
-        indir = TASSO_LCDM_DRN
-        fn_cfg = glob(os.path.join(indir, "*.cfg"))[0]
+    if args.indir == "POBOY":
+        indir = POBOY_DRN_PAT.format(sim_name)
+    elif args.indir == "LCRC":
+        indir = LCRC_DRN_PAT.format(sim_name)
     else:
         indir = args.indir
-        # On LCRC the cfg file is one directory up from the data
-        fn_cfg = glob(os.path.join(os.path.dirname(indir), "*.cfg"))[0]
+    sim_name = "Discovery" + sim_name
 
     start = time()
 
@@ -91,7 +98,7 @@ if __name__ == "__main__":
             ichunk_start = time()
 
             tarr, mahs_for_rank = load_mahs_per_rank(
-                fn_data, fn_cfg, chunknum, nchunks=nchunks, comm=MPI.COMM_WORLD
+                fn_data, sim_name, chunknum, nchunks, comm=MPI.COMM_WORLD
             )
             nhalos_for_rank = mahs_for_rank.shape[0]
             nhalos_tot = comm.reduce(nhalos_for_rank, op=MPI.SUM)
@@ -116,9 +123,7 @@ if __name__ == "__main__":
 
             msg = "\n\nWallclock runtime to fit {0} halos with {1} ranks = {2:.1f} seconds\n\n"
             if rank == 0:
-                print("\nFinished with subvolume {} chunk {}".format(
-                    isubvol, chunknum
-                ))
+                print("\nFinished with subvolume {} chunk {}".format(isubvol, chunknum))
                 runtime = ichunk_end - ichunk_start
                 print(msg.format(nhalos_tot, nranks, runtime))
 
--- scripts/run_discovery_fitter.sh
+++ scripts/run_discovery_fitter.sh
@@ -15,8 +15,5 @@
 source ~/.bash_profile
 conda activate diffhacc
 
-rm -rf /lcrc/project/halotools/DESI_W0WA/diffmah_fits/LCDM/*
-rm -rf /lcrc/project/halotools/DESI_W0WA/diffmah_fits/W0WA/*
-
 cd /home/ahearin/work/random/1113
-mpirun -n 100 python hacc_discovery_sims_diffmah_fitter_script.py /lcrc/group/cosmodata/simulations/DESI_W0WA/LCDM/coreforest/forest /lcrc/project/halotools/DESI_W0WA/diffmah_fits/LCDM diffmah_fits.h5 -istart 0 -iend 1
+mpirun -n 100 python hacc_discovery_sims_diffmah_fitter_script.py LCRC LCDM /lcrc/project/halotools/DESI_W0WA/diffmah_fits/LCDM -istart 0 -iend 1
