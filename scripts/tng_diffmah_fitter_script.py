9d0640d3dfa0817f45216a4e68ae95a93477c10a
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-03-08 16:13:11 +0100

    Fixing correct import for the diffmah_fitter_helpers

--- scripts/tng_diffmah_fitter_script.py
+++ scripts/tng_diffmah_fitter_script.py
@@ -9,7 +9,7 @@ from time import time
 import numpy as np
 from mpi4py import MPI
 
-from diffmah import fitting_helpers as cfh
+from diffmah.fitting_helpers import diffmah_fitter_helpers as cfh
 from diffmah.data_loaders.load_tng_mahs import load_tng_data
 
 TMP_OUTPAT = "tmp_mah_fits_rank_{0}.dat"
