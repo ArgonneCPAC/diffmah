9b776ff3573d9046839f8b5de5a69d738e9580cf
Alex Alarcon <alexalarcongonzalez@gmail.com>
2025-03-18 12:26:16 +0100

    renaming log_mah when loading TNG mah data

--- diffmah/data_loaders/load_tng_mahs.py
+++ diffmah/data_loaders/load_tng_mahs.py
@@ -11,8 +11,8 @@ def load_tng_data(data_drn=BEBOP_TNG):
     tng_t = np.load(os.path.join(data_drn, "tng_cosmic_time.npy"))
     fn = os.path.join(data_drn, "tng_diffmah.npy")
     _halos = np.load(fn)
-    mahs = _halos["mpeakh"]
-    mahs = np.maximum.accumulate(mahs, axis=1)
-    mahs = np.where(mahs == 0.0, 0.0, 10**mahs)
+    log_mahs = _halos["mpeakh"]
+    log_mahs = np.maximum.accumulate(log_mahs, axis=1)
+    mahs = np.where(log_mahs == 0.0, 0.0, 10**log_mahs)
     assert mahs.max() > 1e5
     return tng_t, mahs
