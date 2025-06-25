844cca3fade53b956c307405eba98b60ff13673d
Andrew Hearin <ahearin@anl.gov>
2025-06-25 13:31:12 -0500

    Update job submission script

--- scripts/fitting_scripts/JOBS/run_lj_diffmah_fits.sh
+++ scripts/fitting_scripts/JOBS/run_lj_diffmah_fits.sh
@@ -7,9 +7,9 @@
 #PBS -A galsampler
 
 # allocate {select} nodes, each with {mpiprocs} MPI processes
-#PBS -l select=4:mpiprocs=30
+#PBS -l select=10:mpiprocs=30
 
-#PBS -l walltime=6:00:00
+#PBS -l walltime=10:00:00
 
 # Load software
 source ~/.bash_profile
@@ -17,7 +17,7 @@ conda activate improv311
 
 cd /home/ahearin/work/random/0625/LJ_DIFFMAH_JOBS
 rsync /home/ahearin/work/repositories/python/diffmah/scripts/fitting_scripts/hacc_lastjourney_diffmah_fitter_script.py ./
-mpirun -n 120 python hacc_lastjourney_diffmah_fitter_script.py /lcrc/group/cosmodata/simulations/LastJourney/coretrees/forest /lcrc/project/halotools/LastJourney/diffmah_fits_littleh diffmah_fits.h5 -istart 0 -iend 1
+mpirun -n 300 python hacc_lastjourney_diffmah_fitter_script.py /lcrc/group/cosmodata/simulations/LastJourney/coretrees/forest /lcrc/project/halotools/LastJourney/diffmah_fits_littleh diffmah_fits.h5 -istart 0 -iend 50
 
 rsync /home/ahearin/work/repositories/python/diffmah/scripts/fitting_scripts/unchunk_last_journey_script.py ./
-python unchunk_last_journey_script.py 0 1 -indir /lcrc/project/halotools/LastJourney/diffmah_fits_littleh -outdir /lcrc/project/halotools/LastJourney/diffmah_fits_littleh/unchunked
+python unchunk_last_journey_script.py 0 50 -indir /lcrc/project/halotools/LastJourney/diffmah_fits_littleh -outdir /lcrc/project/halotools/LastJourney/diffmah_fits_littleh/unchunked
